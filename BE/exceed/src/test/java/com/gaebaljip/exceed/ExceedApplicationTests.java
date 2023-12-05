15b936ae0a52444cce5e3b0a82bbbe193cefe207
Sonny <hds650588@gmail.com>
2023-12-05 23:14:29 +0900

    setting : 초기 설정

---
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/ExceedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gaebaljip.exceed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExceedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
