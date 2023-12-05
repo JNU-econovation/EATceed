10cbf127c56f5ff58f02b4ef968b2d1440c6492d
Sonny <hds650588@gmail.com>
2023-12-06 02:52:11 +0900

    feat : 전역 예외 처리 핸들러 구현
    검증, 경로, 데이터 포맷 관련 에러 핸들링 메서드 구현

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/GlobalExceptionHandler.java
@@ -0,0 +1,67 @@
+package com.gaebaljip.exceed.common;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+import java.net.BindException;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * 존재하지 않는 경로로 요청할 때 발생
+     */
+    @ExceptionHandler(NoHandlerFoundException.class)
+    protected ApiResponse<ApiResponse.CustomBody> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        return ApiResponseGenerator.fail(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    /**
+     * javax.validation.Valid 또는 @Validated binding error가 발생할 경우
+     */
+    @ExceptionHandler(BindException.class)
+    protected ApiResponse<ApiResponse.CustomBody> handleBindException(BindException e) {
+        return ApiResponseGenerator.fail(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 주로 @RequestParam enum으로 binding 못했을 경우 발생
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    protected ApiResponse<ApiResponse.CustomBody> handleMethodArgumentTypeMismatchException(
+            MethodArgumentTypeMismatchException e) {
+        return ApiResponseGenerator.fail(e.getMessage(), HttpStatus.BAD_REQUEST);
+    }
+
+    /**
+     * 지원하지 않은 HTTP method 호출 할 경우 발생
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    protected ApiResponse<ApiResponse.CustomBody> handleHttpRequestMethodNotSupportedException(
+            HttpRequestMethodNotSupportedException e) {
+        return ApiResponseGenerator.fail(e.getMessage(), HttpStatus.METHOD_NOT_ALLOWED);
+    }
+
+    /**
+     * 나머지 예외 발생
+     */
+    @ExceptionHandler(Exception.class)
+    protected ApiResponse<ApiResponse.CustomBody> handleException(Exception e) {
+        log.error("Exception", e);
+        return ApiResponseGenerator.fail(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    protected ApiResponse<ApiResponse.CustomBody> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e) {
+        return ApiResponseGenerator.fail(
+                e.getBindingResult().getFieldErrors().get(0).getDefaultMessage(), HttpStatus.BAD_REQUEST);
+    }
+}
