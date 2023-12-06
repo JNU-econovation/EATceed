920f3daf7603ea9aa8cee85e69fd716bed711e35
Sonny <hds650588@gmail.com>
2023-12-07 02:08:54 +0900

    chore : 사용하지 않는 코드 제거

--- BE/exceed/src/test/java/com/gaebaljip/exceed/member/adapter/in/CreateMemberControllerTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/member/adapter/in/CreateMemberControllerTest.java
@@ -1,22 +1,16 @@
 package com.gaebaljip.exceed.member.adapter.in;
 
 import com.gaebaljip.exceed.common.CommonApiTest;
-import com.gaebaljip.exceed.member.application.CreateMemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(CreateMemberController.class)
 class CreateMemberControllerTest extends CommonApiTest {
-
-    @MockBean
-    private CreateMemberService createMemberService;
 
     @Test
     @DisplayName("회원가입 성공")
