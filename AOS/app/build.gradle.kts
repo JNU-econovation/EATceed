d5325cc10e4b789604180fe075aa68b3c20707f7
K_Gs:D <rootachieve6053@gmail.com>
2023-12-08 23:20:51 +0900

    setting/#12 : compile, target SDK 버전 변경
    compile SDK와 Target SDK를 34로 변경했습니다.

--- AOS/app/build.gradle.kts
+++ AOS/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.gaebaljip.exceed"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.gaebaljip.exceed"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
