0bb12433714cf51fb2f2b2f606bad4f61454dd8e
Sonny <hds650588@gmail.com>
2023-12-16 19:21:40 +0900

    feat/#29 : jpa auditing 적용

--- BE/exceed/src/main/java/com/gaebaljip/exceed/common/BaseEntity.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/BaseEntity.java
@@ -6,14 +6,17 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.Column;
+import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
 import java.sql.Timestamp;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
+@EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
 public class BaseEntity {
 
