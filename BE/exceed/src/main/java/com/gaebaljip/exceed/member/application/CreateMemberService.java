e2b4ad3575862bc7bd5d2169949e25edd072da37
Sonny <hds650588@gmail.com>
2023-12-16 19:26:34 +0900

    feat/#29 : MemberConverter 생성 및 CreateMemberService에 적용

--- BE/exceed/src/main/java/com/gaebaljip/exceed/member/application/CreateMemberService.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/member/application/CreateMemberService.java
@@ -1,8 +1,10 @@
 package com.gaebaljip.exceed.member.application;
 
+import com.gaebaljip.exceed.member.adapter.out.persistence.MemberConverter;
 import com.gaebaljip.exceed.member.application.port.in.CreateMemberCommand;
 import com.gaebaljip.exceed.member.application.port.in.CreateMemberUsecase;
 import com.gaebaljip.exceed.member.application.port.out.RecordMemberPort;
+import com.gaebaljip.exceed.member.domain.MemberModel;
 import com.gaebaljip.exceed.member.exception.InvalidAgeException;
 import com.gaebaljip.exceed.member.exception.InvalidGenderException;
 import com.gaebaljip.exceed.member.exception.InvalidHeightException;
@@ -20,12 +22,14 @@ public class CreateMemberService implements CreateMemberUsecase {
     public static final int MINIMUM_AGE = 0;
 
     private final RecordMemberPort recordMemberPort;
+    private final MemberConverter memberConverter;
 
     @Override
     @Transactional
     public void execute(CreateMemberCommand command) {
         validateCommand(command);
-        recordMemberPort.query();
+        MemberModel memberModel = MemberModel.create(command.height(), command.gender(), command.weight(), command.age(), command.activity());
+        recordMemberPort.query(memberConverter.toEntity(memberModel, command.etc()));
     }
 
     private void validateCommand(CreateMemberCommand command) {
