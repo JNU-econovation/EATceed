1d6b7d59863389c2bfb3baf820e99cb641aad8b5
Sonny <hds650588@gmail.com>
2023-12-19 01:13:14 +0900

    test/#29 : DatabaseCleaner를 사용하여 테스트간 격리

--- BE/exceed/src/test/java/com/gaebaljip/exceed/common/CommonApiTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/common/CommonApiTest.java
@@ -3,7 +3,7 @@ package com.gaebaljip.exceed.common;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -11,7 +11,7 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
-@WebMvcTest
+@ActiveProfiles("local")
 public abstract class CommonApiTest {
     @Autowired
     protected MockMvc mockMvc;
