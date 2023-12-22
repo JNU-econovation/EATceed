81a927fb0a7995811db6e26f9962fee61db2a1ca
Sonny <hds650588@gmail.com>
2023-12-22 21:15:45 +0900

    feat/#31 : FoodConverter 생성

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/food/adapter/out/FoodConverter.java
@@ -0,0 +1,26 @@
+package com.gaebaljip.exceed.food.adapter.out;
+
+import com.gaebaljip.exceed.food.domain.FoodModel;;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class FoodConverter {
+
+    public FoodModel toModel(FoodEntity foodEntity) {
+        return FoodModel.builder()
+                .name(foodEntity.getName())
+                .calorie(foodEntity.getCalorie())
+                .carbohydrate(foodEntity.getCarbohydrate())
+                .protein(foodEntity.getProtein())
+                .fat(foodEntity.getFat())
+                .build();
+    }
+
+    public List<FoodModel> toModels(List<FoodEntity> foodEntities) {
+        return foodEntities.stream()
+                .map(this::toModel)
+                .toList();
+    }
+}
