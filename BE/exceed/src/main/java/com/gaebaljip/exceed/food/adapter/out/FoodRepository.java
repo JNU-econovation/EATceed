a31fa1ec20f99fd4ac8426f4ff99b0483386eb29
Sonny <hds650588@gmail.com>
2023-12-22 11:36:19 +0900

    feat/#31 : FoodEntity 생성

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/food/adapter/out/FoodRepository.java
@@ -0,0 +1,6 @@
+package com.gaebaljip.exceed.food.adapter.out;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodRepository extends JpaRepository<FoodEntity, Long> {
+}
