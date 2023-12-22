ddbb3e71a2a180f607d9dc278d67e2b5807b6c6a
Sonny <hds650588@gmail.com>
2023-12-23 01:02:21 +0900

    test/#31 : 반환값이 올바른 지 검증

--- BE/exceed/src/test/java/com/gaebaljip/exceed/meal/adapter/GetMealIntegrationTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/meal/adapter/GetMealIntegrationTest.java
@@ -1,13 +1,15 @@
 package com.gaebaljip.exceed.meal.adapter;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.gaebaljip.exceed.common.ApiResponse;
 import com.gaebaljip.exceed.common.IntegrationTest;
+import com.gaebaljip.exceed.dto.response.GetMealResponse;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 public class GetMealIntegrationTest extends IntegrationTest {
 
@@ -17,7 +19,16 @@ public class GetMealIntegrationTest extends IntegrationTest {
         ResultActions resultActions = mockMvc.perform(get("/v1/meal/2023-12-22")
                 .contentType(MediaType.APPLICATION_JSON));
 
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+
         //then
+        ApiResponse.CustomBody<GetMealResponse> getMealResponseCustomBody = om.readValue(responseBody, new TypeReference<ApiResponse.CustomBody<GetMealResponse>>() {
+        });
+        Double maintainCalorie = getMealResponseCustomBody.getResponse().maintainMeal().maintainCalorie();
+        Double targetCalorie = getMealResponseCustomBody.getResponse().targetMeal().targetCalorie();
+
+        Assertions.assertThat(maintainCalorie).isGreaterThan(0);
+        Assertions.assertThat(targetCalorie).isGreaterThan(maintainCalorie);
         resultActions.andExpect(status().isOk());
     }
 }
