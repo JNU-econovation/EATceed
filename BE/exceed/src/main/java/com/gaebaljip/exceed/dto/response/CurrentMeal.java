65c3dd5b0bfc31a83060de25c0175f3df5a9b08b
Sonny <hds650588@gmail.com>
2023-12-22 21:25:20 +0900

    refactor/#31 : Double 타입 필드들에 3번째자리에서 반올림하도록 @JsonSerialize 추가

--- BE/exceed/src/main/java/com/gaebaljip/exceed/dto/response/CurrentMeal.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/dto/response/CurrentMeal.java
@@ -1,11 +1,17 @@
 package com.gaebaljip.exceed.dto.response;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.gaebaljip.exceed.common.CustomDoubleSerializer;
 import lombok.Builder;
 
 public record CurrentMeal(
+        @JsonSerialize(using = CustomDoubleSerializer.class)
         Double currentCalorie,
+        @JsonSerialize(using = CustomDoubleSerializer.class)
         Double currentCarbohydrate,
+        @JsonSerialize(using = CustomDoubleSerializer.class)
         Double currentProtein,
+        @JsonSerialize(using = CustomDoubleSerializer.class)
         Double currentFat) {
 
     @Builder
