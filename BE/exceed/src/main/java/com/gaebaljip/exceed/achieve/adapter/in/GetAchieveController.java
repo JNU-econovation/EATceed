6f3083a901cc37fe8ff50cafe943ad7147f94cf9
Sonny <hds650588@gmail.com>
2023-12-24 03:01:39 +0900

    refactor/#35 : 해당 월의 달성 여부 보여주는 컨트롤러 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/achieve/adapter/in/GetAchieveController.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/achieve/adapter/in/GetAchieveController.java
@@ -1,11 +1,9 @@
 package com.gaebaljip.exceed.achieve.adapter.in;
 
-import com.gaebaljip.exceed.achieve.application.port.in.GetMonthMealUsecase;
+import com.gaebaljip.exceed.achieve.application.port.in.GetAchieveUsecase;
 import com.gaebaljip.exceed.common.ApiResponse;
 import com.gaebaljip.exceed.common.ApiResponseGenerator;
 import com.gaebaljip.exceed.dto.response.GetAchieveListResponse;
-import com.gaebaljip.exceed.meal.application.GetCurrentMealService;
-import com.gaebaljip.exceed.member.application.GetTargetMealService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -14,7 +12,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 import java.time.LocalDate;
 
 @RestController
@@ -23,14 +20,11 @@ import java.time.LocalDate;
 @RequestMapping("/v1")
 public class GetAchieveController {
 
-    private final GetMonthMealUsecase getMonthMealUsecase;
-    private final GetCurrentMealService getCurrentMealService;
-    private final GetTargetMealService getTargetMealService;
+    private final GetAchieveUsecase getAchieveUsecase;
 
     @GetMapping("/achieve/{date}")
-    public ApiResponse<?> getAchieves(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
-        GetAchieveListResponse achieveListResponse = getMonthMealUsecase.execute(1L, date);
+    public ApiResponse<ApiResponse.CustomBody<GetAchieveListResponse>> getAchieves(@PathVariable @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate date) {
+        GetAchieveListResponse achieveListResponse = getAchieveUsecase.execute(1L, date);
         return ApiResponseGenerator.success(achieveListResponse, HttpStatus.OK);
     }
-
 }
