d570bac8aaa48c2b65c766ba189de4b6171cdf48
Sonny <hds650588@gmail.com>
2023-12-24 03:00:28 +0900

    feat/#35 : 식사가 존재하지 않을 때 예외 생성 및 핸들링 메서드 생성

--- BE/exceed/src/main/java/com/gaebaljip/exceed/common/MessageCode.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/MessageCode.java
@@ -11,7 +11,8 @@ public enum MessageCode {
     INVALID_GENDER("4447", "성별은 1과 0으로만 표현됩니다."),
     INVALID_MEMBER("4448", "존재하지 않는 회원입니다."),
     INVALID_MULTIPLE("4449", "0인분보다 커야하고, 100인분보다 작아야합니다."),
-    INVALID_FOOD("4450", "존재하지 않는 음식입니다.");
+    INVALID_FOOD("4450", "존재하지 않는 음식입니다."),
+    INVALID_MEAL("4451", "존재하지 않는 식사입니다.");
 
 
     private final String code;
