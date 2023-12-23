14c15ad4d61ca268ea4835b3b9b15fb056afecf8
Sonny <hds650588@gmail.com>
2023-12-24 03:03:00 +0900

    test/#35 : 해당 월의 달성 여부 보여주는 기능 통합테스트 구현

---
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/achieve/GetAchieveIntegrationTest.java
@@ -0,0 +1,55 @@
+package com.gaebaljip.exceed.achieve;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.gaebaljip.exceed.common.ApiResponse;
+import com.gaebaljip.exceed.common.IntegrationTest;
+import com.gaebaljip.exceed.dto.response.GetAchieveListResponse;
+import com.gaebaljip.exceed.meal.adapter.out.MealPersistenceAdapter;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDate;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class GetAchieveIntegrationTest extends IntegrationTest {
+
+    @Autowired
+    private MealPersistenceAdapter mealPersistenceAdapter;
+
+    @Test
+    @Transactional
+    void getAchieves() throws Exception {
+        //given
+        String year = "2023";
+        String month = "12";
+        String day = "24";
+        String date = year + "-" + month + "-" + day;
+        LocalDate testDate = LocalDate.of(Integer.parseInt(year), Integer.parseInt(month), Integer.parseInt(day));
+        int comparisonSize = mealPersistenceAdapter.queryForMonthAchievements(1L, testDate).size();
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                get("/v1/achieve/" + date)
+                        .contentType(MediaType.APPLICATION_JSON));
+        //then
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+        System.out.println(responseBody);
+        ApiResponse.CustomBody<GetAchieveListResponse> getAchieveListResponseCustomBody = om.readValue(responseBody, new TypeReference<ApiResponse.CustomBody<GetAchieveListResponse>>() {
+        });
+
+        int comparedSize = getAchieveListResponseCustomBody.getResponse().getAchieves().stream().filter(achieve -> achieve.isVisited().equals(true)).toList().size();
+
+        System.out.println("comparedSize = " + comparedSize);
+        System.out.println("comparisonSize = " + comparisonSize);
+        Assertions.assertThat(comparedSize).isEqualTo(comparisonSize);
+        resultActions.andExpect(status().isOk());
+    }
+
+}
