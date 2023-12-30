a03297d665426b22f3c21bb20e03eda930c0f45e
Sonny <hds650588@gmail.com>
2023-12-30 15:07:39 +0900

    test/#41 : test profile 생성

--- BE/exceed/src/test/java/com/gaebaljip/exceed/ExceedApplicationTests.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/ExceedApplicationTests.java
@@ -1,13 +1,7 @@
 package com.gaebaljip.exceed;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ExceedApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
 }
