2e354a3ab81ba069385e098847b37c31b4e91eea
hwangdaesun <hds650588@gmail.com>
2024-01-04 15:53:00 +0900

    refactor/#47 : GetFood의 imageUri 필드 DailyMeal로 이동

--- BE/exceed/src/test/java/com/gaebaljip/exceed/meal/adapter/GetMealIntegrationTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/meal/adapter/GetMealIntegrationTest.java
@@ -3,18 +3,14 @@ package com.gaebaljip.exceed.meal.adapter;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.gaebaljip.exceed.common.ApiResponse;
 import com.gaebaljip.exceed.common.IntegrationTest;
-import com.gaebaljip.exceed.dto.response.GetMeal;
 import com.gaebaljip.exceed.dto.response.GetMealFoodResponse;
 import com.gaebaljip.exceed.dto.response.GetMealResponse;
-import com.gaebaljip.exceed.meal.application.GetSpecificMealService;
 import com.gaebaljip.exceed.meal.application.port.out.GetPresignedUrlPort;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.ResultActions;
-
-import java.time.LocalDate;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
@@ -37,8 +33,8 @@ public class GetMealIntegrationTest extends IntegrationTest {
         //then
         ApiResponse.CustomBody<GetMealResponse> getMealResponseCustomBody = om.readValue(responseBody, new TypeReference<ApiResponse.CustomBody<GetMealResponse>>() {
         });
-        Double maintainCalorie = getMealResponseCustomBody.getResponse().maintainMeal().maintainCalorie();
-        Double targetCalorie = getMealResponseCustomBody.getResponse().targetMeal().targetCalorie();
+        Double maintainCalorie = getMealResponseCustomBody.getResponse().maintainMeal().calorie();
+        Double targetCalorie = getMealResponseCustomBody.getResponse().targetMeal().calorie();
 
         Assertions.assertThat(maintainCalorie).isGreaterThan(0);
         Assertions.assertThat(targetCalorie).isGreaterThan(maintainCalorie);
@@ -59,8 +55,8 @@ public class GetMealIntegrationTest extends IntegrationTest {
 
         ApiResponse.CustomBody<GetMealFoodResponse> getMealFoodResponseCustomBody = om.readValue(responseBody, new TypeReference<ApiResponse.CustomBody<GetMealFoodResponse>>() {
         });
-        Double maintainCalorie = getMealFoodResponseCustomBody.getResponse().getMealResponse().maintainMeal().maintainCalorie();
-        Double targetCalorie = getMealFoodResponseCustomBody.getResponse().getMealResponse().targetMeal().targetCalorie();
+        Double maintainCalorie = getMealFoodResponseCustomBody.getResponse().getMealResponse().maintainMeal().calorie();
+        Double targetCalorie = getMealFoodResponseCustomBody.getResponse().getMealResponse().targetMeal().calorie();
         int size = getMealFoodResponseCustomBody.getResponse().dailyMeals().size();
         //then
         Assertions.assertThat(maintainCalorie).isGreaterThan(0);
