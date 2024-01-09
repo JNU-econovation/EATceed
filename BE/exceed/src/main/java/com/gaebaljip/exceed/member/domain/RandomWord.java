1c4b7b6b9c8a47e3acbeea34c7d2b0a3e8d632b0
hwangdaesun <hds650588@gmail.com>
2024-01-09 12:41:01 +0900

    feat/#58 : 5글자 랜덤으로 생성

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/member/domain/RandomWord.java
@@ -0,0 +1,28 @@
+package com.gaebaljip.exceed.member.domain;
+
+import lombok.Getter;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+@Getter
+public class RandomWord {
+
+    private static final Integer LENGTH = 5;
+    private final String word;
+
+    public RandomWord() {
+        this.word = generateRandomWord(LENGTH);
+    }
+
+    private String generateRandomWord(int wordLength) {
+        StringBuilder word = new StringBuilder(wordLength);
+
+        for (int i = 0; i < wordLength; i++) {
+            // 'a' (97)부터 시작하여 알파벳 26글자 중 하나를 랜덤으로 선택
+            char letter = (char) ('a' + ThreadLocalRandom.current().nextInt(26));
+            word.append(letter);
+        }
+
+        return word.toString();
+    }
+}
