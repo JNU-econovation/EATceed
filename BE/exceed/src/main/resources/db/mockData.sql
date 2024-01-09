4a85097f15200c64aa4939bc63b67162cd1b53d6
hwangdaesun <hds650588@gmail.com>
2024-01-09 10:46:01 +0900

    feat/#58 : 수정된 스키마에 따른 데이터 변경

--- BE/exceed/src/main/resources/db/mockData.sql
+++ BE/exceed/src/main/resources/db/mockData.sql
@@ -6,8 +6,9 @@ TRUNCATE TABLE FOOD_TB;
 SET FOREIGN_KEY_CHECKS = 1;
 
 INSERT INTO member_tb (created_date, updated_date, member_activity, member_age, member_etc, member_gender,
-                       member_height, member_identification, member_weight)
-VALUES ('2023-12-01 08:00:00', '2023-12-01 08:00:00', 'NOT_ACTIVE', 30, '비고 없음', 1, 175.0, 'user001', 70.0);
+                       member_height, member_login_id, member_password, member_weight)
+VALUES ('2023-12-01 08:00:00', '2023-12-01 08:00:00', 'NOT_ACTIVE', 30, '비고 없음', 1, 175.0, 'testId1234+GUEST',
+        'testId1234+GUEST', 70.0);
 
 INSERT INTO food_tb (food_calorie, food_carbohydrate, food_fat, food_main_category, food_name, food_protein,
                      food_serving_size, food_sub_category)
