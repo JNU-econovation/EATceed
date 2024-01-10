8972ee4f6768b0bcaa6f94445ca93e5d7da99245
hwangdaesun <hds650588@gmail.com>
2024-01-10 22:01:54 +0900

    feat/#62 : findByLoginId 생성

--- BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/out/persistence/MemberRepository.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/out/persistence/MemberRepository.java
@@ -2,5 +2,8 @@ package com.gaebaljip.exceed.member.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+    Optional<MemberEntity> findByLoginId(String loginId);
 }
