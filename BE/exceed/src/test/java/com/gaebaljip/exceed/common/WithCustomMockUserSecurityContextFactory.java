dd6365a6f66aff750fe4f8fcbece9d6672abc572
hwangdaesun <hds650588@gmail.com>
2024-01-10 22:03:25 +0900

    test/#62 : @WithMockGuestUser 구현
    Guest role을 가지고 있는 Mock User 생성

---
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/common/WithCustomMockUserSecurityContextFactory.java
@@ -0,0 +1,22 @@
+package com.gaebaljip.exceed.common;
+
+import com.gaebaljip.exceed.security.CustomUsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.test.context.support.WithSecurityContextFactory;
+
+import java.util.List;
+
+public class WithCustomMockUserSecurityContextFactory implements WithSecurityContextFactory<WithMockGuestUser> {
+
+    @Override
+    public SecurityContext createSecurityContext(WithMockGuestUser annotation) {
+        Long memberId = annotation.memberId();
+        Authentication authentication = new CustomUsernamePasswordAuthenticationToken(null, null, List.of(new SimpleGrantedAuthority("ROLE_GUEST")), memberId);
+        SecurityContext context = SecurityContextHolder.getContext();
+        context.setAuthentication(authentication);
+        return context;
+    }
+}
