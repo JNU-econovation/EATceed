ff9ba0f10b26214e769d9f5cee0f93f8682351fa
hwangdaesun <hds650588@gmail.com>
2024-01-12 00:25:59 +0900

    feat/#62 : AccessDeniedException 핸들링 커스텀 클래스 생성

--- BE/exceed/src/main/java/com/gaebaljip/exceed/security/exception/JwtAccessDeniedHandler.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/security/exception/JwtAccessDeniedHandler.java
@@ -1,19 +1,21 @@
-package com.gaebaljip.exceed.security;
+package com.gaebaljip.exceed.security.exception;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.gaebaljip.exceed.common.ApiResponse;
 import com.gaebaljip.exceed.common.Error;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.stereotype.Component;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
+@RequiredArgsConstructor
 public class JwtAccessDeniedHandler implements org.springframework.security.web.access.AccessDeniedHandler {
+
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException {
         response.setStatus(HttpServletResponse.SC_FORBIDDEN);
