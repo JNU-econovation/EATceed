cb706bfebf3d7e6dc4ca46f30408b7ebebfcbf87
Jang Hong Jun <37576725+ajang369@users.noreply.github.com>
2024-01-14 18:27:12 +0900

    feat/#59 : 각 year과 month에 맞는 days 배치
    LocalDate를 사용하여 년, 월에 해당하는 일을 캘린더 레이아웃 틀에 맞게 배치했습니다.

--- AOS/app/src/main/java/com/gaebaljip/exceed/screens/CalendarScreen.kt
+++ AOS/app/src/main/java/com/gaebaljip/exceed/screens/CalendarScreen.kt
@@ -1,47 +1,54 @@
 package com.gaebaljip.exceed.screens
 
-import android.graphics.Paint
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyHorizontalGrid
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.grid.itemsIndexed
-import androidx.compose.foundation.lazy.grid.rememberLazyGridState
-import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.shape.CornerSize
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.CornerRadius
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.drawscope.Stroke
-import androidx.compose.ui.graphics.nativeCanvas
-import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.gaebaljip.exceed.R
 import com.gaebaljip.exceed.ui.theme.ExceedTheme
+import com.gaebaljip.exceed.ui.theme.Typography
+import org.intellij.lang.annotations.JdkConstants.VerticalScrollBarPolicy
 import java.time.LocalDate
+
+@Composable
+fun CalendarScreen() {
+    ColorBox()
+}
 
 @Composable
 fun ColorBox() {
@@ -52,8 +59,9 @@ fun ColorBox() {
     ) {
         Box(
             modifier = Modifier
-                .fillMaxSize()
-                .padding(bottom = 207.dp)
+//                .padding(bottom = 207.dp)
+                .fillMaxWidth()
+                .height(643.dp)
                 .background(
                     Color(0xFFFDAC53),
                     shape = RoundedCornerShape(
@@ -67,8 +75,8 @@ fun ColorBox() {
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .padding(horizontal = 16.dp)
-                    .padding(top = 64.dp, bottom = 48.dp)
+                    .padding(horizontal = 10.dp)
+                    .padding(top = 64.dp, bottom = 52.dp)
                     .background(Color.White, shape = RoundedCornerShape(5.dp))
 
             ) {
@@ -85,28 +93,36 @@ fun ColorBox() {
 
 @Composable
 fun Header() {
-    Column(modifier = Modifier.padding(8.dp)) {
+    Column(
+        modifier = Modifier.padding(8.dp)
+
+    ) {
         Text(
             text = "2024",
-            fontSize = 18.sp
+            style = Typography.headlineSmall,
         )
         Row(verticalAlignment = Alignment.Bottom) {
+            Box(modifier = Modifier.size(50.dp), contentAlignment = Alignment.BottomCenter) {
+                Text(
+                    text = "1",
+                    textAlign = TextAlign.Center,
+                    style = Typography.headlineLarge
+                )
+            }
             Text(
-                text = "12",
-                fontSize = 40.sp
+                text = "January",
+                modifier = Modifier.padding(start = 8.dp, bottom = 3.dp),
+                style = Typography.headlineMedium,
+                color = colorResource(id = R.color.calendar_month_color)
             )
-            Text(
-                text = "December",
-                fontSize = 20.sp,
-                modifier = Modifier.padding(start = 8.dp)
-            )
-
         }
 
         Text(
             text = "눌러서 이달의 목표를 적어보세요",
             fontSize = 12.sp,
-            modifier = Modifier.padding(top = 8.dp)
+            modifier = Modifier.padding(top = 8.dp, start = 4.dp),
+            style = Typography.displaySmall,
+            color = colorResource(id = R.color.calendar_goal_color)
         )
     }
 }
@@ -116,11 +132,10 @@ fun CalendarLayout(
     modifier: Modifier = Modifier,
     onDayClick: (Int) -> Unit,
     month: String
-    // year: Int,
-    // month_int: Int
+
 ) {
     Column(
-        modifier = Modifier.padding(bottom = 16.dp),
+        modifier = Modifier.padding(bottom = 7.dp),
     ) {
         Header()
 
@@ -129,6 +144,7 @@ fun CalendarLayout(
                 .fillMaxSize()
                 .padding(8.dp)
         ) {
+
             Canvas(
                 modifier = Modifier
                     .fillMaxSize()
@@ -168,10 +184,9 @@ fun CalendarLayout(
                         strokeWidth = 1f
                     )
                 }
-
             }
 
-            CalendarItems()
+            CalendarItems(currentDate = LocalDate.of(2024, 1, 1))
 
         }
     }
@@ -179,46 +194,115 @@ fun CalendarLayout(
 }
 
 @Composable
-fun CalendarItems() {
+fun CalendarItems(
+    currentDate: LocalDate,
+) {
     val dayOfWeek = listOf<String>("S", "M", "T", "W", "T", "F", "S")
+    val firstDayOfWeek by remember { mutableStateOf(currentDate.dayOfWeek.value) }
+    val lastDay by remember { mutableStateOf(currentDate.lengthOfMonth()) }
+    val currentMonthDays by remember { mutableStateOf(IntRange(1, lastDay).toList()) }
 
     Column(
         modifier = Modifier.fillMaxSize()
     ) {
         LazyVerticalGrid(
-            columns = GridCells.Fixed(7)
+            columns = GridCells.Fixed(7),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(64.dp),
+            verticalArrangement = Arrangement.Center,
+            userScrollEnabled = false
 
         ) {
             itemsIndexed(dayOfWeek) { index, days ->
                 val textColor = when (index) {
-                    0 -> Color.Red
-                    dayOfWeek.lastIndex -> Color.Blue
+                    0 -> colorResource(id = R.color.calendar_sunday_color)
+                    dayOfWeek.lastIndex -> colorResource(id = R.color.calendar_saturday_color)
                     else -> Color.Black
                 }
                 Text(
                     text = "$days",
-                    fontSize = 24.sp,
+                    fontSize = 22.sp,
                     color = textColor,
                     textAlign = TextAlign.Center,
                     modifier = Modifier
                         .fillMaxSize()
-                        .padding(top = 16.dp)
                 )
             }
         }
 
+        LazyVerticalGrid(
+            columns = GridCells.Fixed(7),
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.spacedBy(1.dp),
+            horizontalArrangement = Arrangement.spacedBy(1.dp),
+            userScrollEnabled = false
 
+        ) {
+            for (i in 0 until firstDayOfWeek) {
+                item {
+                    Box(
+                        modifier = Modifier
+                            .size(50.dp)
+                            .padding(10.dp),
+                    ) {
+
+                    }
+                }
+            }
+            items(currentMonthDays) { day ->
+                val date = currentDate.withDayOfMonth(day)
+
+                CalendarDays(
+                    date = date,
+                    isToday = date == LocalDate.now(),
+
+                )
+
+            }
+        }
     }
 }
 
 @Composable
-fun HorizontalCalendar(
-    modifier: Modifier = Modifier,
-    currentDate: LocalDate = LocalDate.now(),
-    onSelectedDate: (LocalDate) -> Unit
+fun CalendarDays(
+    date: LocalDate,
+    isToday: Boolean,
 ) {
 
+    Box(
+        modifier = Modifier
+            .size(63.5.dp)
+            .fillMaxWidth(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+
+            Box(
+                contentAlignment = Alignment.Center,
+                modifier = Modifier
+                    .size(32.dp)
+            ) {
+
+
+                Text(
+                    text = date.dayOfMonth.toString(),
+                    style = Typography.bodyMedium,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier.offset(0.dp, 1.dp)
+                )
+
+            }
+
+
+        }
+    }
+
 }
+
 
 @Preview(showBackground = true)
 @Composable
