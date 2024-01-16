c6463a95eab96929f2cd589ac76d16666c3b6a04
K_Gs:D <rootachieve6053@gmail.com>
2024-01-17 01:59:56 +0900

    feat/#82 : Alarm Info class 변경
    Alarm Info class에 불필요한 부분을 제거하고, 필요한 부분을 추가했습니다.

--- AOS/app/src/main/java/com/gaebaljip/exceed/screens/alarm/AlarmInfo.kt
+++ AOS/app/src/main/java/com/gaebaljip/exceed/screens/alarm/AlarmInfo.kt
@@ -8,45 +8,7 @@ data class AlarmInfo(
     val minute: Int,
     val mealType: MealTypeEnum,
     val weekFlag: Int,
-    val isEnabled: Boolean
-) {
-    fun translation(info: AlarmInfoEntity): AlarmInfo {
-        val enum: MealTypeEnum = when (info.mealType) {
-            MealTypeEnum.BREAKFAST.name -> MealTypeEnum.BREAKFAST
-            MealTypeEnum.LAUNCH.name -> MealTypeEnum.LAUNCH
-            MealTypeEnum.DINNER.name -> MealTypeEnum.DINNER
-            MealTypeEnum.SNACK.name -> MealTypeEnum.SNACK
-            else -> {
-                MealTypeEnum.SNACK
-            }
-        }
-        return AlarmInfo(
-            info.id,
-            info.hour,
-            info.minute,
-            enum,
-            info.weekFlag,
-            info.isEnabled
-        )
-    }
-}
-
-data class AlarmInfoEntity(
-    val id: String,
-    val hour: Int,
-    val minute: Int,
-    val mealType: String,
-    val weekFlag: Int,
-    val isEnabled: Boolean
-) {
-    fun translation(info: AlarmInfo): AlarmInfoEntity {
-        return AlarmInfoEntity(
-            info.id,
-            info.hour,
-            info.minute,
-            info.mealType.name,
-            info.weekFlag,
-            info.isEnabled
-        )
-    }
-}
+    val isEnabled: Boolean,
+    val imgDate: String,
+    val imgString: String?
+)
