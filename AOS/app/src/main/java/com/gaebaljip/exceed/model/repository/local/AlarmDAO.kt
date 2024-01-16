04851bcecef3050a9eca24b066c6c74af1b3637e
K_Gs:D <rootachieve6053@gmail.com>
2024-01-17 03:04:56 +0900

    feat/#84 : AlarmDAO 생성
    데이터베이스 접근 오브젝트를 만들었습니다.

---
+++ AOS/app/src/main/java/com/gaebaljip/exceed/model/repository/local/AlarmDAO.kt
@@ -0,0 +1,24 @@
+package com.gaebaljip.exceed.model.repository.local
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+
+@Dao
+interface AlarmDAO {
+    @Query("SELECT * FROM alarmentity WHERE week_flag &(:weekNum) == :weekNum ORDER BY hour asc, minute asc ")
+    fun getTargetWeekALL(weekNum : Int): List<AlarmEntity>
+
+    @Query("UPDATE alarmentity SET hour = (:hour), minute = (:minute) WHERE id == (:id)")
+    fun updateAlarmTimeByID(id: String, hour:Int, minute:Int)
+
+    @Query("UPDATE alarmentity SET img_date = (:date), img_string = (:data) WHERE id == (:id)")
+    fun updateAlarmImageByID(id: String, date:String, data: String?)
+
+    @Insert
+    fun insert(alarm: AlarmEntity)
+
+    @Query("DELETE FROM alarmentity WHERE id == (:id)")
+    fun delete(id: String)
+}
