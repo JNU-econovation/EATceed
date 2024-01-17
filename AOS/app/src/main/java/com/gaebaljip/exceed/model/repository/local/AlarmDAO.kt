d1e3784cb8edfcec95a8e3b27a171202784f034d
K_Gs:D <rootachieve6053@gmail.com>
2024-01-17 15:03:26 +0900

    fix/#84 : AlarmDAO 수정
    AlarmDAO에 누락되었던 파라미터를 추가했습니다.

--- AOS/app/src/main/java/com/gaebaljip/exceed/model/repository/local/AlarmDAO.kt
+++ AOS/app/src/main/java/com/gaebaljip/exceed/model/repository/local/AlarmDAO.kt
@@ -4,14 +4,17 @@ import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.Query
+import com.gaebaljip.exceed.MealTypeEnum
 
 @Dao
 interface AlarmDAO {
+    @Query("SELECT * FROM alarmentity")
+    fun getAllAlarm(): List<AlarmEntity>
     @Query("SELECT * FROM alarmentity WHERE week_flag &(:weekNum) == :weekNum ORDER BY hour asc, minute asc ")
     fun getTargetWeekALL(weekNum : Int): List<AlarmEntity>
 
-    @Query("UPDATE alarmentity SET hour = (:hour), minute = (:minute) WHERE id == (:id)")
-    fun updateAlarmTimeByID(id: String, hour:Int, minute:Int)
+    @Query("UPDATE alarmentity SET hour = (:hour), minute = (:minute), week_flag = (:weekNum), meal_type = (:mealTypeEnum) WHERE id == (:id)")
+    fun updateAlarmByID(id: String, hour:Int, minute:Int, weekNum: Int, mealTypeEnum: MealTypeEnum)
 
     @Query("UPDATE alarmentity SET img_date = (:date), img_string = (:data) WHERE id == (:id)")
     fun updateAlarmImageByID(id: String, date:String, data: String?)
