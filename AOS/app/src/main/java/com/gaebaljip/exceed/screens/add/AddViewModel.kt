c71cee07cf18babcab807082dc10e48decde0f1d
K_Gs:D <rootachieve6053@gmail.com>
2024-01-17 17:14:45 +0900

    feat/#84 : AddViewModel 에 메서드 생성
    AddScreen에서 사용하기 위해 메서드를 생성했습니다.

--- AOS/app/src/main/java/com/gaebaljip/exceed/screens/add/AddViewModel.kt
+++ AOS/app/src/main/java/com/gaebaljip/exceed/screens/add/AddViewModel.kt
@@ -1,14 +1,32 @@
 package com.gaebaljip.exceed.screens.add
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.gaebaljip.exceed.MealTypeEnum
+import com.gaebaljip.exceed.model.repository.MainRepository
 import com.gaebaljip.exceed.screens.alarm.AlarmInfo
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
+import java.util.Calendar
 import java.util.UUID
 
 class AddViewModel : ViewModel() {
+    private val mainRepository = MainRepository
     private val _dataList = MutableStateFlow<List<AlarmInfo>>(listOf())
     val dataList : StateFlow<List<AlarmInfo>>
         get() = _dataList
+
+    fun getListCurrentWeek(){
+        val cal = Calendar.getInstance()
+
+        val weekNum = 1 shl (cal.get(Calendar.DAY_OF_WEEK) - 1)
+        viewModelScope.launch(Dispatchers.IO) {
+            _dataList.update {
+                mainRepository.getAlarmListByCurrentWeek(weekNum)
+            }
+        }
+    }
 }
