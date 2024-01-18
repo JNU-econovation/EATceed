7489551d48697488617167f8d3363d834d600f90
hwangdaesun <hds650588@gmail.com>
2024-01-18 21:47:56 +0900

    feat/#78 : request와 response 캐싱하는 필터

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/log/CustomServletWrappingFilter.java
@@ -0,0 +1,23 @@
+package com.gaebaljip.exceed.common.log;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class CustomServletWrappingFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        ContentCachingRequestWrapper wrappedRequest = new ContentCachingRequestWrapper(request);
+        ContentCachingResponseWrapper wrappedResponse = new ContentCachingResponseWrapper(response);
+        filterChain.doFilter(wrappedRequest, wrappedResponse);
+        wrappedResponse.copyBodyToResponse();
+    }
+}
