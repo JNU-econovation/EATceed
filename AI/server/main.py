fa3a37585b7f516f6401808f037eb90587ce3368
Kyeong6 <wwnss1411@gmail.com>
2024-01-18 13:14:40 +0900

    feat: 챗봇 API 구현(/v1/chat/)

--- AI/server/main.py
+++ AI/server/main.py
@@ -1,6 +1,7 @@
 import openai
 from fastapi import FastAPI
 from pydantic import BaseModel
+from starlette import status
 
 app = FastAPI(
     title="Exceed Food-Chatbot",
@@ -16,3 +17,18 @@ chat_responses = []
 chat_log = [{'role':'system',
             'content':"Different situations, people, and emotions will have different food cravings,\
                   and you're good at making food recommendations based on those characteristics."}]
+
+@app.post("/v1/chat/", status_code=status.HTTP_201_CREATED)
+async def chat(user_input: UserInput):
+    chat_log.append({'role': 'user', 'content': user_input.user_input})
+    chat_responses.append(user_input.user_input)
+
+    responses = openai.chat.completions.create(
+        model='gpt-3.5-turbo',
+        messages=chat_log,
+        temperature=0.6
+    )
+
+
+    bot_response = responses.choices[0].message.content
+    chat_log.append({'role': 'assistant', 'content': bot_response})
