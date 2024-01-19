1da336a64542539bd68b5dda71391487edf506a5
hwangdaesun <hds650588@gmail.com>
2024-01-20 01:25:10 +0900

    refactor/#98 : getDocumentRequest 메서드에 URI 스펙 추가

--- BE/exceed/src/test/java/com/gaebaljip/exceed/common/util/ApiDocumentUtil.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/common/util/ApiDocumentUtil.java
@@ -8,7 +8,12 @@ import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
 public interface ApiDocumentUtil {
     static OperationRequestPreprocessor getDocumentRequest() {
         // Request Spec을 정렬해서 출력해줌
-        return preprocessRequest(prettyPrint());
+        return preprocessRequest(
+                modifyUris()
+                .scheme("http")
+                .host("43.203.82.233")
+                .port(8080)
+                ,prettyPrint());
     }
 
     static OperationResponsePreprocessor getDocumentResponse() {
