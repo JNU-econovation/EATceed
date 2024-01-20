3df91fe092e9fb8d919b2f9908197786fdebcf93
Kyeong6 <wwnss1411@gmail.com>
2024-01-20 23:42:33 +0900

    feat: 챗봇 prompt, log 등 채팅 요소 설정

--- AI/server/main.py
+++ AI/server/main.py
@@ -2,3 +2,11 @@ import openai
 from fastapi import FastAPI, HTTPException, Depends
 from typing import Annotated
 from pydantic import BaseModel
+
+
+app = FastAPI(
+    title="Exceed Food-Chatbot",
+    description="API that use fine-tuning ChatGPT Model as a chatbot",
+    version="1.0.0"
+)
+
