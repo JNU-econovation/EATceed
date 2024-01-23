a0a8880cd91f2152d0e23db32ca911b7706d1cc8
hwangdaesun <hds650588@gmail.com>
2024-01-23 13:25:41 +0900

    feat/#127 : 음식 검색 기능 + 페이지네이션 API

--- BE/exceed/src/main/java/com/gaebaljip/exceed/food/application/GetFoodService.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/food/application/GetFoodService.java
@@ -1,6 +1,5 @@
 package com.gaebaljip.exceed.food.application;
 
-import com.gaebaljip.exceed.dto.response.GetFood;
 import com.gaebaljip.exceed.dto.response.GetPageableFood;
 import com.gaebaljip.exceed.food.application.out.LoadFoodPort;
 import com.gaebaljip.exceed.food.application.in.GetFoodQuery;
@@ -18,19 +17,9 @@ import java.util.stream.Collectors;
 public class GetFoodService implements GetFoodQuery {
 
     private final LoadFoodPort loadFoodPort;
-
     @Override
-    public GetFood execute(Long foodId) {
-        FoodModel foodModel = loadFoodPort.query(foodId);
-        return GetFood.builder()
-                .id(foodModel.getId())
-                .name(foodModel.getName())
-                .build();
-    }
-
-    @Override
-    public Slice<GetPageableFood> execute(String lastFoodName, int size) {
-        Slice<FoodModel> foodModels = loadFoodPort.query(lastFoodName, size);
+    public Slice<GetPageableFood> execute(String lastFoodName, String keyword, int size) {
+        Slice<FoodModel> foodModels = loadFoodPort.query(lastFoodName, size, keyword);
         List<GetPageableFood> pageableFoods = foodModels.getContent().stream().map(foodModel -> new GetPageableFood(foodModel.getId(), foodModel.getName())).collect(Collectors.toList());
         return new SliceImpl<>(pageableFoods, foodModels.getPageable(), foodModels.hasNext());
     }
