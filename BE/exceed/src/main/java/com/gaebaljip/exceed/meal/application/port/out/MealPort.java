bccbac186fb3282d65236338ff0641881592f03d
hwangdaesun <hds650588@gmail.com>
2024-03-01 00:05:02 +0900

    refactor/#137 : 인퍼에이스명 변경
    저장하는 메서드는 command로 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/meal/application/port/out/MealPort.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/meal/application/port/out/MealPort.java
@@ -4,6 +4,6 @@ import com.gaebaljip.exceed.meal.adapter.out.MealEntity;
 import org.springframework.stereotype.Component;
 
 @Component
-public interface RecordMealPort {
-    MealEntity query(MealEntity mealEntity);
+public interface MealPort {
+    MealEntity command(MealEntity mealEntity);
 }
