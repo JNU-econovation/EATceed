990262c7098a2e6e3854cb5527dec1af60c88d96
hwangdaesun <hds650588@gmail.com>
2024-03-01 00:53:43 +0900

    fix/#137 : 아직 아무 식사도 하지 않을 예외 케이스 고려

--- BE/exceed/src/main/java/com/gaebaljip/exceed/meal/application/GetCurrentMealService.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/meal/application/GetCurrentMealService.java
@@ -1,10 +1,10 @@
 package com.gaebaljip.exceed.meal.application;
 
+import com.gaebaljip.exceed.dto.request.DailyMeal;
 import com.gaebaljip.exceed.dto.response.CurrentMeal;
-import com.gaebaljip.exceed.meal.domain.MealModel;
-import com.gaebaljip.exceed.meal.application.port.out.LoadDailyMealPort;
+import com.gaebaljip.exceed.meal.domain.Meal;
+import com.gaebaljip.exceed.meal.application.port.out.DailyMealPort;
 import com.gaebaljip.exceed.meal.application.port.in.GetCurrentMealQuery;
-import com.gaebaljip.exceed.meal.domain.MealsModel;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,18 +16,28 @@ import java.util.List;
 @RequiredArgsConstructor
 public class GetCurrentMealService implements GetCurrentMealQuery {
 
-    private final LoadDailyMealPort loadDailyMealPort;
+    public static final double ZERO = 0.0;
+    private final DailyMealPort dailyMealPort;
 
     @Override
     @Transactional(readOnly = true)
     public CurrentMeal execute(Long memberId) {
-        List<MealModel> mealModels = loadDailyMealPort.queryMealsForDate(memberId, LocalDate.now());
-        MealsModel mealsModel = new MealsModel(mealModels);
-        return CurrentMeal.builder()
-                .calorie(mealsModel.calculateCurrentCalorie())
-                .carbohydrate(mealsModel.calculateCurrentCarbohydrate())
-                .fat(mealsModel.calculateCurrentFat())
-                .protein(mealsModel.calculateCurrentProtein())
-                .build();
+        List<Meal> meals = dailyMealPort.query(new DailyMeal(memberId, LocalDate.now()));
+        if(meals.isEmpty()){
+            return CurrentMeal.builder()
+                    .calorie(ZERO)
+                    .carbohydrate(ZERO)
+                    .fat(ZERO)
+                    .protein(ZERO)
+                    .build();
+        }else {
+            com.gaebaljip.exceed.meal.domain.DailyMeal dailyMeal = new com.gaebaljip.exceed.meal.domain.DailyMeal(meals);
+            return CurrentMeal.builder()
+                    .calorie(dailyMeal.calculateCurrentCalorie())
+                    .carbohydrate(dailyMeal.calculateCurrentCarbohydrate())
+                    .fat(dailyMeal.calculateCurrentFat())
+                    .protein(dailyMeal.calculateCurrentProtein())
+                    .build();
+        }
     }
 }
