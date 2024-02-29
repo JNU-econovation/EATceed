ffd1f40e4801b1536abe02ae8956a20df3dea8c1
hwangdaesun <hds650588@gmail.com>
2024-03-01 00:09:09 +0900

    refactor/#137 : 각 메서드가 영속성 계층에서 애플리케이션 계층으로 넘어갈 때 도메인을 반환하도록 변경

--- BE/exceed/src/main/java/com/gaebaljip/exceed/meal/adapter/out/MealPersistenceAdapter.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/meal/adapter/out/MealPersistenceAdapter.java
@@ -1,45 +1,45 @@
 package com.gaebaljip.exceed.meal.adapter.out;
 
-import com.gaebaljip.exceed.achieve.application.port.out.LoadMonthMealPort;
-import com.gaebaljip.exceed.achieve.domain.DailyRecord;
+import com.gaebaljip.exceed.nutritionist.application.port.out.MonthlyMealPort;
 import com.gaebaljip.exceed.config.DateConverter;
-import com.gaebaljip.exceed.meal.application.port.out.LoadDailyMealPort;
-import com.gaebaljip.exceed.meal.application.port.out.RecordMealPort;
-import com.gaebaljip.exceed.meal.domain.MealModel;
+import com.gaebaljip.exceed.dto.request.DailyMeal;
+import com.gaebaljip.exceed.dto.request.MonthlyMeal;
+import com.gaebaljip.exceed.meal.application.port.out.DailyMealPort;
+import com.gaebaljip.exceed.meal.application.port.out.MealPort;
+import com.gaebaljip.exceed.meal.domain.Meal;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
-
 import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
-public class MealPersistenceAdapter implements RecordMealPort, LoadDailyMealPort, LoadMonthMealPort {
+public class MealPersistenceAdapter implements MealPort, DailyMealPort, MonthlyMealPort {
 
     private final MealRepository mealRepository;
     private final MealConverter mealConverter;
     private final int FIRST_DAY = 1;
 
     @Override
-    public MealEntity query(MealEntity mealEntity) {
+    public MealEntity command(MealEntity mealEntity) {
         return mealRepository.save(mealEntity);
     }
 
     @Override
-    public List<MealModel> queryMealsForDate(Long memberId, LocalDate date) {
-        Timestamp today = DateConverter.toEpochSecond(date);
-        Timestamp tomorrow = DateConverter.toEpochSecond(date.plusDays(1));
-        List<MealEntity> mealEntities = mealRepository.findAllTodayMeal(today, tomorrow, memberId);
-        return mealConverter.toModels(mealEntities);
+    public List<Meal> query(DailyMeal dailyMeal) {
+        Timestamp today = DateConverter.toEpochSecond(dailyMeal.date());
+        Timestamp tomorrow = DateConverter.toEpochSecond(dailyMeal.date().plusDays(1));
+        List<MealEntity> mealEntities = mealRepository.findAllTodayMeal(today, tomorrow, dailyMeal.memberId());
+        return mealConverter.toMeals(mealEntities);
     }
 
     @Override
-    public List<DailyRecord> queryForMonthAchievements(Long memberId, LocalDate date) {
-        Timestamp startOfMonth = DateConverter.toEpochSecond(LocalDate.of(date.getYear(), date.getMonth(), FIRST_DAY));
-        Timestamp endOfMonth = DateConverter.toEpochSecond(LocalDate.of(date.getYear(), date.getMonth(), getDayOfMonth(date)));
-        List<MealEntity> monthMealEntities = mealRepository.findAllMonthMeal(startOfMonth, endOfMonth, memberId);
-        return mealConverter.toDailyAchieves(monthMealEntities);
+    public List<Meal> query(MonthlyMeal monthlyMeal) {
+        Timestamp startOfMonth = DateConverter.toEpochSecond(LocalDate.of(monthlyMeal.date().getYear(), monthlyMeal.date().getMonth(), FIRST_DAY));
+        Timestamp endOfMonth = DateConverter.toEpochSecond(LocalDate.of(monthlyMeal.date().getYear(), monthlyMeal.date().getMonth(), getDayOfMonth(monthlyMeal.date())));
+        List<MealEntity> mealEntities = mealRepository.findMealsByMemberAndMonth(startOfMonth, endOfMonth, monthlyMeal.memberId());
+        return mealConverter.toMeals(mealEntities);
     }
 
     private int getDayOfMonth(LocalDate date) {
