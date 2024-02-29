fa1af2f146cfdbf663d861d6def3645b5d00e587
hwangdaesun <hds650588@gmail.com>
2024-03-01 00:54:35 +0900

    refactor/#137  : 기타 변경사항

--- BE/exceed/src/main/java/com/gaebaljip/exceed/dto/response/GetAchieveListResponse.java
+++
@@ -1,9 +0,0 @@
-package com.gaebaljip.exceed.dto.response;
-
-import lombok.Builder;
-
-import java.util.List;
-
-public record GetAchieveListResponse(List<GetAchieve> getAchieves) {
-
-}
--- BE/exceed/src/main/java/com/gaebaljip/exceed/dto/response/GetMeal.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/dto/response/GetMeal.java
@@ -1,9 +1,6 @@
 package com.gaebaljip.exceed.dto.response;
 
-import com.gaebaljip.exceed.meal.domain.MealType;
 import lombok.Builder;
-
-import java.time.LocalTime;
 import java.util.List;
 
 public record GetMeal(
