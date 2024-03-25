123b7bf4127cd0d85c411407f9fa93b6d3093dc7
hwangdaesun <hds650588@gmail.com>
2024-03-25 16:35:56 +0900

    refactor/#156 : memberId를 사용해서 MemberEntity를 조회하도록 변경

--- BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/out/persistence/MemberRepository.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/out/persistence/MemberRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
-    Optional<MemberEntity> findByLoginId(String loginId);
 }
