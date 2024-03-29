cac9057461a70d62e39583c523b5970a0246a802
hwangdaesun <hds650588@gmail.com>
2024-03-29 21:49:44 +0900

    feat/#158 : MemberEntity에 checked 컬럼 추가 및 각 컬럼 제약 조건 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/out/persistence/MemberEntity.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/out/persistence/MemberEntity.java
@@ -24,33 +24,36 @@ public class MemberEntity extends BaseEntity {
     @Column(name = ENTITY_PREFIX + "_PK", nullable = false)
     private Long id;
 
-    @Column(name = ENTITY_PREFIX + "_HEIGHT", nullable = false)
+    @Column(name = ENTITY_PREFIX + "_HEIGHT")
     private double height;
 
-    @Column(name = ENTITY_PREFIX + "_GENDER", nullable = false, columnDefinition = "tinyint")
+    @Column(name = ENTITY_PREFIX + "_GENDER", columnDefinition = "tinyint")
     private Integer gender;
 
-    @Column(name = ENTITY_PREFIX + "_WEIGHT", nullable = false)
+    @Column(name = ENTITY_PREFIX + "_WEIGHT")
     private Double weight;
 
-    @Column(name = ENTITY_PREFIX + "_AGE", nullable = false)
+    @Column(name = ENTITY_PREFIX + "_AGE")
     private Integer age;
 
-    @Column(name = ENTITY_PREFIX + "_LOGIN_ID", nullable = false)
-    private String loginId;
+    @Column(name = ENTITY_PREFIX + "_EMAIL", nullable = false, unique = true)
+    private String email;
 
     @Column(name = ENTITY_PREFIX + "_PASSWORD", nullable = false)
     private String password;
 
-    @Column(name = ENTITY_PREFIX + "_ETC", nullable = false)
+    @Column(name = ENTITY_PREFIX + "_CHECKED", nullable = false)
+    private Boolean checked;
+
+    @Column(name = ENTITY_PREFIX + "_ETC")
     private String etc;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = ENTITY_PREFIX + "_ACTIVITY", nullable = false)
+    @Column(name = ENTITY_PREFIX + "_ACTIVITY")
     private Activity activity;
 
     @Enumerated(EnumType.STRING)
-    @Column(name = ENTITY_PREFIX + "_ROLE", nullable = false)
+    @Column(name = ENTITY_PREFIX + "_ROLE")
     private MemberRole role;
 
     // todo : MemberEntity의 History 테이블 필요
