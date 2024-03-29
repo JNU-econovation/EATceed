3d14955fb8d1159848b8b16e54c99923e0374553
hwangdaesun <hds650588@gmail.com>
2024-03-29 21:57:41 +0900

    feat/#158 : MemberEntity에 email 컬럼과 checked 컬럼 검사
    email이 없을 경우 checked가 false일 경우 고려

--- BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/out/persistence/MemberRepository.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/out/persistence/MemberRepository.java
@@ -1,8 +1,13 @@
 package com.gaebaljip.exceed.member.adapter.out.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface MemberRepository extends JpaRepository<MemberEntity, Long> {
+
+    Boolean existsByEmail(String email);
+
+    @Query("select m.checked from MemberEntity m where m.email = :email")
+    Boolean findCheckedByEmail(@Param("email") String email);
 }
