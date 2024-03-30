22cfff034e3dbc2c8ca690ba1be3ca9a3ad7d0d7
hwangdaesun <hds650588@gmail.com>
2024-03-30 18:28:42 +0900

    refactor/#158 : template안의 링크에 code와 email 추가

--- BE/exceed/src/main/java/com/gaebaljip/exceed/common/MailTemplate.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/MailTemplate.java
@@ -5,5 +5,7 @@ public class MailTemplate {
 
     public static final String SIGN_UP_TITLE = "Eatceed 회원가입 인증 메일";
     public static final String SIGN_UP_MAIL_CONTEXT = "signupLink";
+    public static final String SIGN_UP_CHECK_COOD = "checkCode";
+    public static final String SIGN_UP_EMAIL = "email";
     public static final String REPLY_TO_SIGN_UP_MAIL_URL = "http://localhost:8080/v1/members/checked";
 }
--- BE/exceed/src/main/java/com/gaebaljip/exceed/member/application/SendSignupEmailService.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/member/application/SendSignupEmailService.java
@@ -1,5 +1,6 @@
 package com.gaebaljip.exceed.member.application;
 
+import com.gaebaljip.exceed.common.Encryption;
 import com.gaebaljip.exceed.common.MailTemplate;
 import com.gaebaljip.exceed.member.application.port.in.SendEmailCommand;
 import com.gaebaljip.exceed.member.application.port.in.SendEmailUsecase;
@@ -9,16 +10,24 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.context.Context;
 
+import java.util.UUID;
+
 @Service
 @RequiredArgsConstructor
 @Qualifier("SendSignupEmailService")
 public class SendSignupEmailService implements SendEmailUsecase {
 
     private final EmailPort emailPort;
+    private final Encryption encryption;
     @Override
     public void execute(SendEmailCommand sendEmailCommand) {
+
+        String uuid = UUID.randomUUID().toString();
+        String code = encryption.encrypt(uuid);
         Context context = new Context();
-        context.setVariable(MailTemplate.SIGN_UP_MAIL_CONTEXT,MailTemplate.REPLY_TO_SIGN_UP_MAIL_URL);
+        context.setVariable(MailTemplate.SIGN_UP_MAIL_CONTEXT, MailTemplate.REPLY_TO_SIGN_UP_MAIL_URL);
+        context.setVariable(MailTemplate.SIGN_UP_CHECK_COOD, "&code=" + code);
+        context.setVariable(MailTemplate.SIGN_UP_EMAIL, "?email=" + sendEmailCommand.email());
         emailPort.sendEmail(
                 sendEmailCommand.email(),
                 MailTemplate.SIGN_UP_TITLE,
