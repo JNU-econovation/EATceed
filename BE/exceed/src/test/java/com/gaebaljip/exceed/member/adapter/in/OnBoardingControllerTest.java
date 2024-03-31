4343af1c2b564a7f28270be59b9126b45f98886b
hwangdaesun <hds650588@gmail.com>
2024-03-31 23:02:28 +0900

    test/#166 : 온보딩 기능 변경으로 인한 테스트 수정

--- BE/exceed/src/test/java/com/gaebaljip/exceed/member/adapter/in/OnBoardingControllerTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/member/adapter/in/OnBoardingControllerTest.java
@@ -1,21 +1,16 @@
 package com.gaebaljip.exceed.member.adapter.in;
 
 import com.gaebaljip.exceed.common.CommonApiTest;
-import com.gaebaljip.exceed.dto.response.OnBoardingMember;
-import com.gaebaljip.exceed.member.application.port.in.OnBoardingMemberUsecase;
-import com.gaebaljip.exceed.member.application.port.in.OnBoardingMemberCommand;
-import com.gaebaljip.exceed.security.domain.JwtManager;
+import com.gaebaljip.exceed.common.WithMockUser;
+import com.gaebaljip.exceed.dto.request.OnBoardingMemberRequest;
+import com.gaebaljip.exceed.member.application.OnBoardingMemberService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -23,27 +18,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class OnBoardingControllerTest extends CommonApiTest {
 
     @MockBean
-    private OnBoardingMemberUsecase onBoardingMemberUsecase;
-    @MockBean
-    private JwtManager jwtManager;
-
+    private OnBoardingMemberService onBoardingMemberService;
     @Test
-    @DisplayName("회원가입 성공")
-    void createGuest() throws Exception {
+    @DisplayName("온보딩 성공")
+    @WithMockUser
+    void onBoarding() throws Exception {
         //given
 
-        CreateGuestTestRequest request = new CreateGuestTestRequest(
-                171, 1, 61, 25, "NOT_ACTIVE", "뭐든 잘 먹습니다.");
-
-        given(onBoardingMemberUsecase.execute(any(OnBoardingMemberCommand.class))).willReturn(OnBoardingMember.builder()
-                .loginId("testId1234")
-                .password("testPassword1234")
-                .build());
+        OnBoardingMemberRequest request = new OnBoardingMemberRequest(
+                171.0, 1, 61.0, 65.0,26, "NOT_ACTIVE", "뭐든 잘 먹습니다.");
 
         //when
 
+
         ResultActions resultActions = mockMvc.perform(
-                MockMvcRequestBuilders.post("/v1/members-guest")
+                MockMvcRequestBuilders.post("/v1/members/detail")
                         .content(om.writeValueAsString(request))
                         .contentType(MediaType.APPLICATION_JSON));
 
@@ -54,26 +43,29 @@ class OnBoardingControllerTest extends CommonApiTest {
     }
 
     @Test
-    @DisplayName("유효하지 않은 activity 입력시 오류 발생")
-    void createGuest_invalidActivity() throws Exception {
+    @DisplayName("온보딩-유효하지 않은 activity 입력시 오류 발생")
+    @WithMockUser
+    void onBoarding_invalidActivity() throws Exception {
         //given
 
         String invalidValue = "ACTIVE";
 
-        CreateGuestTestRequest request = new CreateGuestTestRequest(
-                171, 1, 61, 25, invalidValue, "뭐든 잘 먹습니다.");
+        OnBoardingMemberRequest request = new OnBoardingMemberRequest(
+                171.0, 1, 61.0, 65.0,26, invalidValue, "뭐든 잘 먹습니다.");
 
         //when
+
         ResultActions resultActions = mockMvc.perform(
-                RestDocumentationRequestBuilders.post("/v1/members-guest")
+                MockMvcRequestBuilders.post("/v1/members/detail")
                         .content(om.writeValueAsString(request))
+
                         .contentType(MediaType.APPLICATION_JSON));
 
 
         //then
         resultActions.andExpect(status().isBadRequest());
         resultActions.andExpect(jsonPath("$.error.reason").value(invalidValue + "는 올바르지 않은 값입니다."));
-        resultActions.andDo(document("create-guest-fail"));
+        resultActions.andDo(document("onBoarding-fail"));
     }
 
 
