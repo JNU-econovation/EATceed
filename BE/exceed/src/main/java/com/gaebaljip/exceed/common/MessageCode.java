e6e865479eb909e074b25bcd800217e595543937
hwangdaesun <hds650588@gmail.com>
2024-04-01 14:01:14 +0900

    feat/#169 : 비밀번호 불일치 예외 생성 및 핸들링

--- BE/exceed/src/main/java/com/gaebaljip/exceed/common/MessageCode.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/MessageCode.java
@@ -17,6 +17,8 @@ public enum MessageCode {
     INVALID_JWT("5000", "잘못된 토큰입니다."),
     EXPIRED_JWT("5001", "만료된 토큰입니다."),
     UNSUPPORTED_JWT("5002", "지원되지 않는 토큰입니다."),
+
+    PASSWORD_MISMATCH("5003", "비밀번호가 일치하지 않습니다."),
     INSUFFICIENT_MEALS("6000","하루 식사는 최소 한 끼 이상 제공되어야 합니다."),
     NOT_SAME_DATE("6001","하루 식사는 같은 날짜입니다."),
     ALREADY_Checked_EMAIL("7001", "이미 등록된 이메일입니다."),
