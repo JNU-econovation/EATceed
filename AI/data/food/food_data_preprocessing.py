abeeb59865bac69059f2a7e2ba810ed946b3521a
Kyeong6 <wwnss1411@gmail.com>
2024-04-01 16:12:27 +0900

    feat: 음식 데이터 전처리 파일 생성 및 라이브러리 설치

---
+++ AI/data/food/food_data_preprocessing.py
@@ -0,0 +1,18 @@
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,py:light
+#     text_representation:
+#       extension: .py
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: eatceed2
+#     language: python
+#     name: eatceed2
+# ---
+
+# ## 1. 라이브러리 설치
+
+import pandas as pd
