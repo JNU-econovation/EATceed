06e23b70b4e28a8196a8fb2adef2756631905562
Kyeong6 <wwnss1411@gmail.com>
2024-04-01 16:21:45 +0900

    feat: 공공 데이터 포털 csv 파일 업로드

--- AI/data/food/food_data_preprocessing.py
+++ AI/data/food/food_data_preprocessing.py
@@ -26,3 +26,26 @@ import os
 load_dotenv()
 
 file_path = os.environ.get('RAW_DATA_PATH')
+# -
+
+# ## 2. 데이터 수집
+# - 데이터 수집 후 필요 속성 null이 없는 데이터셋 구축
+
+# ### 공공 데이터 포털
+# - https://www.data.go.kr/data/15100070/standard.do?recommendDataYn=Y
+
+# csv파일 Load
+try:
+    df = pd.read_csv(file_path + '전국통합식품영양성분정보 표준데이터.csv', encoding='utf-8')
+except UnicodeDecodeError:
+    try:
+        df = pd.read_csv(file_path + '전국통합식품영양성분정보 표준데이터.csv', encoding='cp949')
+    except Exception as e:
+        print(f"Error: {e}")
+
+# ### 식품의약품안전처
+# - https://various.foodsafetykorea.go.kr/nutrient/
+
+
+
+
