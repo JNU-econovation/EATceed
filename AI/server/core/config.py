bc319730292bf1cb75b0f834d50d2d1840eb0fba
Kyeong6 <wwnss1411@gmail.com>
2024-04-07 21:00:08 +0900

    feat: Router 테스트 진행 파일 생성

--- AI/server/core/config.py
+++ AI/server/core/config.py
@@ -14,5 +14,5 @@ class Settings:
     JWT_SECRET = os.getenv("JWT_SECRET")
 
     DB_URL=f"mysql+pymysql://{RDS_DATABASE_USERNAME}:{RDS_DATABASE_PASSWORD}@{RDS_DATABASE_ENDPOINT}:{RDS_PORT}/{RDS_DB_NAME}?charset=utf8mb4"
-
+    
 settings = Settings()
