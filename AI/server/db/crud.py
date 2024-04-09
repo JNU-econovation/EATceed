ee6c98da7f43c80b3d9be71a1694a03fd4d183ce
Kyeong6 <wwnss1411@gmail.com>
2024-04-09 15:52:30 +0900

    feat: crud.py 필요 라이브러리 설정

--- AI/server/db/crud.py
+++ AI/server/db/crud.py
@@ -1,1 +1,6 @@
 # DB 관련 CRUD 작업 함수 정의
+from sqlalchemy.orm import Session
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose import JWTError, jwt
+
