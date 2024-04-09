b5af717c1e149c3d5db018b2496ea035fb15862c
Kyeong6 <wwnss1411@gmail.com>
2024-04-09 16:02:42 +0900

    feat: DB에서 memberId 정보를 가져오는 함수 get_current_member 정의

--- AI/server/db/crud.py
+++ AI/server/db/crud.py
@@ -6,5 +6,25 @@ from jose import JWTError, jwt
 
 from core.config import settings
 
+# 인증을 위한 환경변수 세팅
 JWT_SECRET = settings.JWT_SECRET
 ALGORITHM = "HS256"
+
+# bearer token 해석 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# DB에서 member id 가져오기
+async def get_current_member(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-authenticate" : "Bearer"}
+    )
+    try:
+        decoded_payload = jwt.decode(token, JWT_SECRET, algorithms=[ALGORITHM])
+        member_id: int = decoded_payload.get("memberId")
+        if member_id is None:
+            raise credentials_exception
+        return member_id
+    except JWTError:
+        raise credentials_exception
