b686e087d475c9d8fd1dc90375874302a65a02b2
Kyeong6 <wwnss1411@gmail.com>
2024-04-09 15:28:23 +0900

    feat: DB 연결을 위한 테이블 구성 파일 models.py 라이브러리 설치

--- AI/server/db/models.py
+++ AI/server/db/models.py
@@ -1,1 +1,6 @@
 # 데이터베이스 Table 구성
+from sqlalchemy import Column, Integer, String, DateTime, Float, Text, ForeignKey, BigInteger, Boolean, Double
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from db.database import engine
+from sqlalchemy.orm import registry
