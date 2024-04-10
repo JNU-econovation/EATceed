d6d06d74393893aed98b1508997ee082c8e14e07
Jin Hyuk <ekrrdj07@gmail.com>
2024-04-10 14:38:32 +0900

    feat/#184 : UpdateMemberRequest 추가

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/dto/request/UpdateMemberRequest.java
@@ -0,0 +1,14 @@
+package com.gaebaljip.exceed.dto.request;
+
+import com.gaebaljip.exceed.common.annotation.Enum;
+import com.gaebaljip.exceed.member.domain.Activity;
+import com.gaebaljip.exceed.member.domain.Gender;
+
+public record UpdateMemberRequest(Double height,
+                                  @Enum(enumClass = Gender.class) String gender,
+                                  Double weight,
+                                  Double targetWeight,
+                                  Integer age,
+                                  @Enum(enumClass = Activity.class) String activity,
+                                  String etc) {
+}
