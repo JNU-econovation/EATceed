3418aa15092c2de154df9065ad99b0d99e79d2dc
Kyeong6 <wwnss1411@gmail.com>
2024-04-10 15:02:52 +0900

    feat: DB CRUD Test를 위한 crud_test 함수 정의

--- AI/server/db/crud.py
+++ AI/server/db/crud.py
@@ -1,6 +1,19 @@
 # DB CRUD 함수 정의
 from sqlalchemy.orm import Session
 from datetime import datetime
-from db import models
+from db.models import EatHabits
 
-# def crud_test(db: Session, )
+# DB 연결 Test CRUD 
+def crud_test(db: Session, member_id: int, flag: bool, weight_prediction: str, weight_advice: str):
+    created_date = datetime.now()
+    eat_habits = EatHabits(
+        CREATED_DATE=created_date,
+        MEMBER_FK = member_id,
+        FLAG = flag,
+        WEIGHT_PREDICTION = weight_prediction,
+        WEIGHT_ADVICE = weight_advice
+    )
+    db.add(eat_habits)
+    db.commit()
+    db.refresh(eat_habits)
+    return eat_habits
