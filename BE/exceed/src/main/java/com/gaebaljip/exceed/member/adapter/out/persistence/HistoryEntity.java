782547e13efeff97a2829d6be5877e2e9f84f266
Jin Hyuk <ekrrdj07@gmail.com>
2024-04-10 14:34:23 +0900

    feat/#184 : HistoryEntity 추가

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/out/persistence/HistoryEntity.java
@@ -0,0 +1,51 @@
+package com.gaebaljip.exceed.member.adapter.out.persistence;
+
+import com.gaebaljip.exceed.common.BaseEntity;
+import com.gaebaljip.exceed.member.domain.Activity;
+import com.gaebaljip.exceed.member.domain.Gender;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@ToString
+@Entity
+@Table(name = HistoryEntity.ENTITY_PREFIX + "_TB")
+@Builder(toBuilder = true)
+public class HistoryEntity extends BaseEntity {
+    public static final String ENTITY_PREFIX = "HISTORY";
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = ENTITY_PREFIX + "_PK", nullable = false)
+    private Long id;
+
+    @Column(name = ENTITY_PREFIX + "_HEIGHT", nullable = false)
+    private double height;
+
+    @Convert(converter = GenderConvert.class)
+    @Column(name = ENTITY_PREFIX + "_GENDER", columnDefinition = "tinyint")
+    private Gender gender;
+
+    @Column(name = ENTITY_PREFIX + "_WEIGHT", nullable = false)
+    private double weight;
+
+    @Column(name = ENTITY_PREFIX + "_AGE")
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = ENTITY_PREFIX + "_ACTIVITY")
+    private Activity activity;
+
+    @CreatedDate
+    @Column(name = ENTITY_PREFIX + "_CREATED_AT", nullable = false)
+    private LocalDateTime createdAt;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "MEMBER_FK", referencedColumnName = "MEMBER_PK")
+    private MemberEntity memberEntity;
+}
