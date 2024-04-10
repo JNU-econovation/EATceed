056e7be106f2b5ebf9aa5ae42a9ca76937c0ddcd
Jin Hyuk <ekrrdj07@gmail.com>
2024-04-10 14:19:52 +0900

    feat/#184 : 마커 인터페이스 추가

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/annotation/PersistenceAdapter.java
@@ -0,0 +1,15 @@
+package com.gaebaljip.exceed.common.annotation;
+
+import org.springframework.core.annotation.AliasFor;
+import org.springframework.stereotype.Component;
+
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Documented
+@Component
+public @interface PersistenceAdapter {
+    @AliasFor(annotation = Component.class)
+    String value() default "";
+}
