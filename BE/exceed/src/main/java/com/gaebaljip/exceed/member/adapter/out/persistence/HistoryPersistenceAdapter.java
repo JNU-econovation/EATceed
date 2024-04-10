b4f234c99e5b1e9f33908abcfbcd5afe6fa17e03
Jin Hyuk <ekrrdj07@gmail.com>
2024-04-10 17:25:03 +0900

    feat/#184 : HistoryPersistenceAdapter 마커 어노테이션 적용

--- BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/out/persistence/HistoryPersistenceAdapter.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/out/persistence/HistoryPersistenceAdapter.java
@@ -1,12 +1,13 @@
 package com.gaebaljip.exceed.member.adapter.out.persistence;
 
+import com.gaebaljip.exceed.common.annotation.PersistenceAdapter;
 import org.springframework.stereotype.Component;
 
 import com.gaebaljip.exceed.member.application.port.out.HistoryPort;
 
 import lombok.RequiredArgsConstructor;
 
-@Component
+@PersistenceAdapter
 @RequiredArgsConstructor
 public class HistoryPersistenceAdapter implements HistoryPort {
     private final HistoryRepository historyRepository;
