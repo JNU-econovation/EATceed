318014df822cb1eac66366af47408ee5bcfbe174
Jin Hyuk <ekrrdj07@gmail.com>
2024-05-02 16:57:24 +0900

    feat/#198 : 음식 검색어 자동완성 기능

--- BE/exceed/src/main/java/com/gaebaljip/exceed/food/adapter/in/GetFoodController.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/food/adapter/in/GetFoodController.java
@@ -1,15 +1,13 @@
 package com.gaebaljip.exceed.food.adapter.in;
 
-import org.springframework.data.domain.Slice;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import com.gaebaljip.exceed.common.ApiResponse;
 import com.gaebaljip.exceed.common.ApiResponseGenerator;
-import com.gaebaljip.exceed.dto.response.GetPageableFood;
+import com.gaebaljip.exceed.dto.response.GetFoodsResponse;
 import com.gaebaljip.exceed.food.application.in.GetFoodQuery;
 
-import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
@@ -26,13 +24,21 @@ public class GetFoodController {
     private final GetFoodQuery getFoodQuery;
 
     /** 음식 정보 무한 스크롤 조회 */
+    //    @GetMapping("/foods")
+    //    @Operation(summary = "음식 정보 조회", description = "음식 정보를 무한 스크롤로 조회한다.")
+    //    public ApiResponse<ApiResponse.CustomBody<Slice<GetPageableFood>>> getFoods(
+    //            @RequestParam(value = "lastFoodName", required = false) String lastFoodName,
+    //            @RequestParam(value = "keyword", required = false) String keyword,
+    //            @RequestParam(defaultValue = "10") int size) {
+    //        Slice<GetPageableFood> getPageableFoods = getFoodQuery.execute(lastFoodName, keyword,
+    // size);
+    //        return ApiResponseGenerator.success(getPageableFoods, HttpStatus.OK);
+    //    }
+
     @GetMapping("/foods")
-    @Operation(summary = "음식 정보 조회", description = "음식 정보를 무한 스크롤로 조회한다.")
-    public ApiResponse<ApiResponse.CustomBody<Slice<GetPageableFood>>> getFoods(
-            @RequestParam(value = "lastFoodName", required = false) String lastFoodName,
-            @RequestParam(value = "keyword", required = false) String keyword,
-            @RequestParam(defaultValue = "10") int size) {
-        Slice<GetPageableFood> getPageableFoods = getFoodQuery.execute(lastFoodName, keyword, size);
-        return ApiResponseGenerator.success(getPageableFoods, HttpStatus.OK);
+    public ApiResponse<ApiResponse.CustomBody<GetFoodsResponse>> getFoods(
+            @RequestParam String prefix) {
+        GetFoodsResponse response = getFoodQuery.execute(prefix);
+        return ApiResponseGenerator.success(response, HttpStatus.OK);
     }
 }
--- BE/exceed/src/main/java/com/gaebaljip/exceed/food/application/in/GetFoodQuery.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/food/application/in/GetFoodQuery.java
@@ -3,9 +3,12 @@ package com.gaebaljip.exceed.food.application.in;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
 
+import com.gaebaljip.exceed.dto.response.GetFoodsResponse;
 import com.gaebaljip.exceed.dto.response.GetPageableFood;
 
 @Component
 public interface GetFoodQuery {
     Slice<GetPageableFood> execute(String lastFoodId, String keyword, int size);
+
+    GetFoodsResponse execute(String prefix);
 }
