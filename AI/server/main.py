8ec368b5bb541040fb6f0a3cfa55cfba66390c03
Kyeong6 <wwnss1411@gmail.com>
2024-05-06 19:07:57 +0900

    feat: Swagger 인증 구현(main.py)

--- AI/server/main.py
+++ AI/server/main.py
@@ -1,19 +1,50 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, status
+from typing import Annotated
+import secrets
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+from fastapi.openapi.utils import get_openapi
+from fastapi.responses import HTMLResponse, UJSONResponse
+from fastapi.openapi.docs import get_swagger_ui_html, get_redoc_html
 from routers.test import *
-from routers.docs import *
+from core.config import settings
+
 
 app = FastAPI(
     title="EATceed",
-    description="API that use food classification and eating habits analysis"
+    description="API that use food classification and eating habits analysis",
+    docs_url=None,
+    redoc_url=None,
+    openapi_url="/api/openapi.json",
+    default_response_class=UJSONResponse,
 )
 
-# Router 설정
- 
+# HTTP 기본 인증을 사용하는 Security 객체 생성
+security = HTTPBasic()
+
+def get_current_username(credentials: HTTPBasicCredentials = Depends(security)) -> str:
+    correct_username = secrets.compare_digest(credentials.username, settings.ADMIN_USERNAME)
+    correct_password = secrets.compare_digest(credentials.password, settings.ADMIN_PASSWORD)
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return credentials.username
+
+@app.get("/docs", response_class=HTMLResponse)
+async def get_docs(username: str = Depends(get_current_username)) -> HTMLResponse:
+    return get_swagger_ui_html(openapi_url="/api/openapi.json", title="docs")
+
+
+@app.get("/redoc", response_class=HTMLResponse)
+async def get_redoc(username: str = Depends(get_current_username)) -> HTMLResponse:
+    return get_redoc_html(openapi_url="/api/openapi.json", title="redoc")
+
+
+# Router 설정 
 # test.py
 app.include_router(test) 
-
-# docs.py
-app.include_router(docs)
 
 
 # API Server Test
--- AI/server/routers/docs.py
+++ AI/server/routers/docs.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, HTTPException,status, APIRouter
+from fastapi import Depends, HTTPException, status, APIRouter
 from fastapi.security import HTTPBasic, HTTPBasicCredentials
 import secrets
 from starlette.responses import HTMLResponse
@@ -8,34 +8,35 @@ from typing import Annotated
 import logging
 from core.config import settings
 
+
 # 로그 메시지
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
-
-docs = APIRouter(prefix='/docs')
 
 # HTTP 기본 보안 스키마 초기화
 security = HTTPBasic()
 
 # 관리자 인증 함수
-def get_admin(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+def get_admin(credentials: HTTPBasicCredentials = Depends(security)):
     correct_username = secrets.compare_digest(credentials.username, settings.ADMIN_USERNAME)
     correct_password = secrets.compare_digest(credentials.password, settings.ADMIN_PASSWORD)
     if not (correct_username and correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect email or password",
+            detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Basic"},
         )
     return ""
 
+docs = APIRouter()
+
 @docs.get("/", include_in_schema=False)
 async def get_documentation(admin: str = Depends(get_admin)):
-    return get_swagger_ui_html(openapi_url="/docs/openapi.json", title="docs")
+    return get_swagger_ui_html(openapi_url="/docs/openapi.json", title="EATceed")
 
 @docs.get("/openapi.json", include_in_schema=False)
-async def openapi(app: FastAPI, admin: str = Depends(get_admin)):
-    return get_openapi(title=app.title, version=app.version, routes=app.routes)
+async def openapi(admin: str = Depends(get_admin)):
+    return get_openapi(title="EATceed", version="1.0.0", routes=docs.routes)
 
 
 
