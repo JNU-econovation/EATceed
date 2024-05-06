d5b871f6fd2808b78fccfe20cbb270e7847b542d
Kyeong6 <wwnss1411@gmail.com>
2024-05-06 16:55:48 +0900

    feat: Swagger 문서 관리자 인증 함수 get_admin 구현

--- AI/server/routers/docs.py
+++ AI/server/routers/docs.py
@@ -4,6 +4,7 @@ import secrets
 from starlette.responses import HTMLResponse
 from fastapi.openapi.utils import get_openapi
 from fastapi.openapi.docs import get_swagger_ui_html
+from typing import Annotated
 import logging
 
 # 로그 메시지
@@ -11,5 +12,20 @@ logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 
 docs = APIRouter(prefix='/docs')
+
+# HTTP 기본 보안 스키마 초기화
+security = HTTPBasic()
+
+# 관리자 인증 함수
+def get_admin(credentials: Annotated[HTTPBasicCredentials, Depends(security)]):
+    correct_username = secrets.compare_digest(credentials.username, "user")
+    correct_password = secrets.compare_digest(credentials.password, "password")
+    if not (correct_username and correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect email or password",
+            headers={"WWW-Authenticate": "Basic"},
+        )
+    return ""
 
 
