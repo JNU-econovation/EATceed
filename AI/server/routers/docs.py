dc2cec283c9592a210118992ef5171b639dc0199
Kyeong6 <wwnss1411@gmail.com>
2024-05-06 16:47:51 +0900

    feat: docs.py에 필요한 library 설정

--- AI/server/routers/docs.py
+++ AI/server/routers/docs.py
@@ -1,1 +1,6 @@
 from fastapi import FastAPI, Depends, HTTPException,status
+from fastapi.security import HTTPBasic, HTTPBasicCredentials
+import secrets
+from starlette.responses import HTMLResponse
+from fastapi.openapi.utils import get_openapi
+from fastapi.openapi.docs import get_swagger_ui_html
