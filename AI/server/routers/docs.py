ad7baea06e9e03355ad044ef9ced5aeb77611dbd
Kyeong6 <wwnss1411@gmail.com>
2024-05-06 16:48:34 +0900

    feat: docs.py log message 설정

--- AI/server/routers/docs.py
+++ AI/server/routers/docs.py
@@ -4,3 +4,8 @@ import secrets
 from starlette.responses import HTMLResponse
 from fastapi.openapi.utils import get_openapi
 from fastapi.openapi.docs import get_swagger_ui_html
+import logging
+
+# 로그 메시지
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
