e6e47beaa10643bac8baa6f95fec49f807eb6cc3
hwangdaesun <hds650588@gmail.com>
2024-05-24 21:06:34 +0900

    feat/#259 : EventPublisherAspect 개선

--- BE/exceed/src/main/java/com/gaebaljip/exceed/member/application/DeleteMemberService.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/member/application/DeleteMemberService.java
@@ -3,6 +3,7 @@ package com.gaebaljip.exceed.member.application;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.gaebaljip.exceed.common.annotation.EventPublisherStatus;
 import com.gaebaljip.exceed.common.event.DeleteMemberEvent;
 import com.gaebaljip.exceed.common.event.Events;
 import com.gaebaljip.exceed.member.adapter.out.persistence.MemberEntity;
@@ -18,6 +19,7 @@ public class DeleteMemberService implements DeleteMemberUseCase {
 
     @Override
     @Transactional
+    @EventPublisherStatus
     public void execute(Long memberId) {
         MemberEntity memberEntity = memberPort.query(memberId);
         Events.raise(DeleteMemberEvent.from(memberEntity));
