1a5a282fda74adac0201e77da9002a3541271a37
Kyeong6 <wwnss1411@gmail.com>
2024-05-29 00:55:32 +0900

    fix: diet_analysis router 요청 방식 변경으로 인한 수정 진행

--- AI/server/routers/diet_analysis.py
+++ AI/server/routers/diet_analysis.py
@@ -2,7 +2,7 @@
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.orm import Session
 from db.database import get_db
-from db.crud import get_user_data, create_eat_habits
+from db.crud import get_user_data, create_eat_habits, get_latest_eat_habits
 from apis.api import analyze_diet, weight_predict
 from auth.decoded_token import get_current_member
 import logging
@@ -23,43 +23,11 @@ logger = logging.getLogger(__name__)
 @analysis.get("/analyze_diet", tags=['analysis'])
 def full_analysis_route(db: Session = Depends(get_db), member_id: int = Depends(get_current_member)):
     try:
-        user_data = get_user_data(db, member_id)
-        
-        # 체중 예측
-        weight_result = weight_predict(user_data)
-        user_data['weight_change'] = weight_result
-        
-        # 각 프롬프트에 대해 분석 수행
-        prompt_types = ['health_advice', 'weight_carbo', 'weight_fat', 'weight_protein']
-        analysis_results = {}
-        for prompt_type in prompt_types:
-            result = analyze_diet(prompt_type, user_data, weight_result)
-            analysis_results[prompt_type] = result
-
-        # logger.debug(f"Member ID {member_id} requested analysis")
-        # logger.debug(f"Analysis results: {analysis_results}")
-
-        # DB에 결과값 저장
-        create_eat_habits(
-            db=db,
-            member_id=member_id,
-            weight_prediction=weight_result,
-            advice_carbo=analysis_results['weight_carbo'],
-            advice_protein=analysis_results['weight_protein'],
-            advice_fat=analysis_results['weight_fat'],
-            synthesis_advice=analysis_results['health_advice'],
-            flag=True
-        )
-
-        logger.info(f"Insert success")
-
-        return {
-            'weight_predict': weight_result,
-            'analysis': analysis_results
-        }
-    except ValueError as e:
-        logger.error(f"Value error during analysis: {e}")
-        raise HTTPException(status_code=400, detail=str(e))
+        latest_eat_habits = get_latest_eat_habits(db, member_id)
+        if latest_eat_habits:
+            return latest_eat_habits
+        else:
+            HTTPException(status_code=404, detail="No analysis found for the given member_id")
     except Exception as e:
-        logger.error(f"Error during analysis: {e}")
-        raise HTTPException(status_code=500, detail="Analysis failed")
+        logger.error(f"Error fetching analysis for member_id: {member_id} - {e}")
+        raise HTTPException(status_code=500, detail="Error fetching analysis")
