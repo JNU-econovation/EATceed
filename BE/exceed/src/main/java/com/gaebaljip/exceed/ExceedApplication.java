a1b393cf6269f546f72f5910828a0bed82ac1047
Jin Hyuk <ekrrdj07@gmail.com>
2024-05-28 23:06:21 +0900

    feat/#276 : 이메일 발송 비동기 처 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/ExceedApplication.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/ExceedApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
-@EnableAsync
 public class ExceedApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExceedApplication.class, args);
