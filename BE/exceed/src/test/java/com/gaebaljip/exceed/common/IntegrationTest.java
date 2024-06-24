9780ec4fded7515915595fb98c32022d51cfd8bb
hwangdaesun <hds650588@gmail.com>
2024-06-24 16:38:10 +0900

    test/#214 : ContainerTest 클래스 상속

--- BE/exceed/src/test/java/com/gaebaljip/exceed/common/IntegrationTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/common/IntegrationTest.java
@@ -7,11 +7,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -19,13 +16,9 @@ import org.springframework.web.filter.CharacterEncodingFilter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
-@ExtendWith(DatabaseClearExtension.class)
 @ExtendWith(RestDocumentationExtension.class)
-@Sql("classpath:db/testData.sql")
-@ActiveProfiles("test")
-public abstract class IntegrationTest {
+public abstract class IntegrationTest extends ContainerTest {
 
     @Autowired protected MockMvc mockMvc;
 
