3c8fb28923ff3ce72d481de2ff2b9c667f3f0073
Jin Hyuk <ekrrdj07@gmail.com>
2024-06-30 21:14:37 +0900

    feat/#331 : ValidationMessage 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/common/ValidationMessage.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/ValidationMessage.java
@@ -6,4 +6,8 @@ public class ValidationMessage {
 
     public static final String INVALID_PASSWORD =
             "비밀번호 형식이 올바르지 않습니다. 비밀번호는 최소 6글자 이상이며, 영어와 특수문자를 포함해야 합니다.";
+
+    public static final String NOT_NULL = "입력해 주세요";
+    public static final String NOT_BLANK = "입력해 주세요";
+    public static final String MIN_0 = "0이상 입력해 주세요";
 }
