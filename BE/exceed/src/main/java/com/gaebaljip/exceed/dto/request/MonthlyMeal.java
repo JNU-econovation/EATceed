832d71c71901eec601195ee9544f1f03fe213297
Jin Hyuk <ekrrdj07@gmail.com>
2024-06-30 21:14:00 +0900

    feat/#331 : MonthlyMeal 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/dto/request/MonthlyMeal.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/dto/request/MonthlyMeal.java
@@ -1,5 +1,9 @@
 package com.gaebaljip.exceed.dto.request;
 
+import com.gaebaljip.exceed.common.ValidationMessage;
+
+import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
 
-public record MonthlyMeal(Long memberId, LocalDateTime date) {}
+public record MonthlyMeal(@NotNull(message = "memberId을 " + ValidationMessage.NOT_NULL) Long memberId,
+                          @NotNull(message = "날짜를 " + ValidationMessage.NOT_NULL) LocalDateTime date) {}
