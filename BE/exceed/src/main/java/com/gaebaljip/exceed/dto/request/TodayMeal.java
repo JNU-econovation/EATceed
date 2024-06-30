6dca4f4fd9a0aa5f8834534d5576251ab1f15842
Jin Hyuk <ekrrdj07@gmail.com>
2024-06-30 21:14:20 +0900

    feat/#331 : TodayMeal 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/dto/request/TodayMeal.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/dto/request/TodayMeal.java
@@ -2,9 +2,13 @@ package com.gaebaljip.exceed.dto.request;
 
 import java.time.LocalDateTime;
 
+import com.gaebaljip.exceed.common.ValidationMessage;
 import lombok.Builder;
 
-public record TodayMeal(Long memberId, LocalDateTime date) {
+import javax.validation.constraints.NotNull;
+
+public record TodayMeal(@NotNull(message = "memberId을 " + ValidationMessage.NOT_NULL) Long memberId,
+                        @NotNull(message = "날짜를 " + ValidationMessage.NOT_NULL) LocalDateTime date) {
 
     @Builder
     public TodayMeal {}
