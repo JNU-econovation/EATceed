86bfd9d80f825f4e97f5c75ad91af15b223d935a
Jin Hyuk <ekrrdj07@gmail.com>
2024-06-30 21:14:11 +0900

    feat/#331 : OnBoardingMemberRequest 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/dto/request/OnBoardingMemberRequest.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/dto/request/OnBoardingMemberRequest.java
@@ -1,17 +1,21 @@
 package com.gaebaljip.exceed.dto.request;
 
+import com.gaebaljip.exceed.common.ValidationMessage;
 import com.gaebaljip.exceed.common.annotation.Enum;
 import com.gaebaljip.exceed.member.domain.Activity;
 import com.gaebaljip.exceed.member.domain.Gender;
 
 import lombok.Builder;
 
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 public record OnBoardingMemberRequest(
-        Double height,
+        @NotNull(message = "키를 " + ValidationMessage.NOT_NULL) @Min(value = 0, message = "키는 " + ValidationMessage.MIN_0) Double height,
         @Enum(enumClass = Gender.class) String gender,
-        Double weight,
-        Double targetWeight,
-        Integer age,
+        @NotNull(message = "몸무게를 " + ValidationMessage.NOT_NULL) @Min(value = 0, message = "몸무게는 " + ValidationMessage.MIN_0)Double weight,
+        @NotNull(message = "목표 몸무게를 " + ValidationMessage.NOT_NULL) @Min(value = 0, message = "목표 몸무게는 " + ValidationMessage.MIN_0)Double targetWeight,
+        @NotNull(message = "나이를 " + ValidationMessage.NOT_NULL) @Min(value = 0, message = "나이는 " + ValidationMessage.MIN_0)Integer age,
         @Enum(enumClass = Activity.class) String activity,
         String etc) {
 
