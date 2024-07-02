b686ffe930a9b53b16dab6aba35cf5a324650f2a
hwangdaesun <hds650588@gmail.com>
2024-07-02 12:46:26 +0900

    refactor/#321 : 사용하지 않는 메서드 제거

--- BE/exceed/src/main/java/com/gaebaljip/exceed/food/application/port/in/GetFoodQuery.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/food/application/port/in/GetFoodQuery.java
@@ -1,16 +1,12 @@
 package com.gaebaljip.exceed.food.application.port.in;
 
-import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Component;
 
 import com.gaebaljip.exceed.dto.response.GetFoodResponse;
 import com.gaebaljip.exceed.dto.response.GetFoodsAutoResponse;
-import com.gaebaljip.exceed.dto.response.GetPageableFoodDTO;
 
 @Component
 public interface GetFoodQuery {
-    Slice<GetPageableFoodDTO> execute(String lastFoodId, String keyword, int size);
-
     GetFoodsAutoResponse execute(String prefix);
 
     GetFoodResponse execute(Long foodId);
