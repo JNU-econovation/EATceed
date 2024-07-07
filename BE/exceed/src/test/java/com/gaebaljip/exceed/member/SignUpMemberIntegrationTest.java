e47b2777a07746f5f29c15da0c35d46f398e046f
hwangdaesun <hds650588@gmail.com>
2024-07-07 20:18:29 +0900

    test/#360 : 회원가입 성공 및 실패시 통합 테스트 수행

--- BE/exceed/src/test/java/com/gaebaljip/exceed/member/SignUpMemberIntegrationTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/member/SignUpMemberIntegrationTest.java
@@ -1,46 +1,96 @@
 package com.gaebaljip.exceed.member;
 
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.event.ApplicationEvents;
+import org.springframework.test.context.event.RecordApplicationEvents;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.gaebaljip.exceed.common.ApiResponse;
 import com.gaebaljip.exceed.common.IntegrationTest;
+import com.gaebaljip.exceed.common.event.SendEmailEvent;
+import com.gaebaljip.exceed.common.redis.RedisUtils;
+import com.gaebaljip.exceed.dto.request.SignUpMemberRequest;
 import com.gaebaljip.exceed.member.adapter.out.persistence.MemberEntity;
 import com.gaebaljip.exceed.member.adapter.out.persistence.MemberRepository;
+import com.gaebaljip.exceed.member.exception.MemberError;
 
+@RecordApplicationEvents
 class SignUpMemberIntegrationTest extends IntegrationTest {
 
-    @Autowired private MemberRepository memberRepository;
+    @Autowired ApplicationEvents events;
+    @Autowired RedisUtils redisUtils;
+    @Autowired MemberRepository memberRepository;
+
+    @AfterEach
+    void tearDown() {
+        redisUtils.flushDB();
+    }
 
     @Test
-    void verifyEmailChecked() throws Exception {
-
+    @DisplayName(
+            "회원가입 : 성공"
+                    + "회원 생성 여부 확인"
+                    + "이메일 인증 이벤트 발생 여부 확인"
+                    + "redis에 인증 코드 저장 여부 확인"
+                    + "회원가입 API 응답 코드 확인")
+    void when_signUpMember_expected_createMemberAndPublishEvent() throws Exception {
         // given
-        String email = "aaa@naver.com";
-        MemberEntity memberEntity =
-                MemberEntity.builder().email(email).password("aaaa1234@@").checked(false).build();
-        memberRepository.save(memberEntity); // memberId = 2인 member 생성
-
+        SignUpMemberRequest request = getSignUpMemberRequest();
         // when
-
         ResultActions resultActions =
                 mockMvc.perform(
-                        MockMvcRequestBuilders.get("/v1/members/email/checked")
-                                .param("email", email)
+                        MockMvcRequestBuilders.post("/v1/members")
+                                .content(om.writeValueAsString(request))
                                 .contentType(MediaType.APPLICATION_JSON));
 
         // then
-        resultActions.andExpect(status().isOk());
-        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
-        ApiResponse.CustomBody<Boolean> verifyEmailChecked =
-                om.readValue(responseBody, new TypeReference<ApiResponse.CustomBody<Boolean>>() {});
-        Assertions.assertEquals(verifyEmailChecked.getResponse(), false);
+        resultActions.andExpect(status().isCreated());
+
+        long count = events.stream(SendEmailEvent.class).count();
+        assertAll(
+                () ->
+                        assertEquals(
+                                memberRepository.findByEmail(request.email()).get().getChecked(),
+                                false),
+                () -> assertEquals(1, count),
+                () -> assertNotNull(redisUtils.getData(request.email())));
+    }
+
+    @Test
+    @DisplayName("회원가입 : 실패" + "이미 가입된 이메일인 경우")
+    void when_signUpMember_then_Fail() throws Exception {
+
+        // given
+        SignUpMemberRequest request = getSignUpMemberRequest();
+        MemberEntity memberEntity =
+                MemberEntity.builder().email(request.email()).password(request.password()).build();
+        memberRepository.save(memberEntity);
+
+        // when
+        ResultActions resultActions =
+                mockMvc.perform(
+                        MockMvcRequestBuilders.post("/v1/members")
+                                .content(om.writeValueAsString(request))
+                                .contentType(MediaType.APPLICATION_JSON));
+
+        // then
+        resultActions.andExpectAll(
+                status().isBadRequest(),
+                jsonPath("$.error.reason").value(MemberError.ALREADY_SIGN_UP_MEMBER.getReason()));
+    }
+
+    private SignUpMemberRequest getSignUpMemberRequest() {
+        return SignUpMemberRequest.builder()
+                .email("sonny1234444!@gmail.com")
+                .password("sonny123!@@")
+                .build();
     }
 }
