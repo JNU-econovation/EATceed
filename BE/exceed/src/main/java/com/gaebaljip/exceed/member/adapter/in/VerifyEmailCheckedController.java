3dee16deff3a240c6bef54f10d2f401426f94cad
hwangdaesun <hds650588@gmail.com>
2024-07-07 20:19:26 +0900

    refactor/#360 : SignUpMemberController 클래스에 있던 API들 다른 클래스로 분리

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/member/adapter/in/VerifyEmailCheckedController.java
@@ -0,0 +1,34 @@
+package com.gaebaljip.exceed.member.adapter.in;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gaebaljip.exceed.common.ApiResponse;
+import com.gaebaljip.exceed.common.ApiResponseGenerator;
+import com.gaebaljip.exceed.member.application.port.in.VerifyEmailCheckedUsecase;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1")
+@SecurityRequirement(name = "access-token")
+@Tag(name = "[회원가입]")
+public class VerifyEmailCheckedController {
+
+    private final VerifyEmailCheckedUsecase verifyEmailCheckedUsecase;
+
+    @Operation(summary = "회원 가입후 이메일 인증 여부 확인", description = "회원 가입후 이메일 인증 여부 확인한다.")
+    @GetMapping("/members/email/checked")
+    public ApiResponse<ApiResponse.CustomBody<Boolean>> verifyEmailChecked(
+            @RequestParam(value = "email") String email) {
+        Boolean isChecked = verifyEmailCheckedUsecase.execute(email);
+        return ApiResponseGenerator.success(isChecked, HttpStatus.OK);
+    }
+}
