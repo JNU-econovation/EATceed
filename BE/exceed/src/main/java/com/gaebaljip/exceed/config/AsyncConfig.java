27b29006c8624b1125080a4d1b5765156295c562
hwangdaesun <hds650588@gmail.com>
2024-07-07 20:22:51 +0900

    refactor/#360 : 테스트 프로필 제외

--- BE/exceed/src/main/java/com/gaebaljip/exceed/config/AsyncConfig.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/config/AsyncConfig.java
@@ -3,6 +3,7 @@ package com.gaebaljip.exceed.config;
 import java.util.concurrent.Executor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
@@ -11,6 +12,7 @@ import com.gaebaljip.exceed.member.exception.MailSendException;
 
 @EnableAsync
 @Configuration
+@Profile("!test")
 public class AsyncConfig implements AsyncConfigurer {
     @Override
     public Executor getAsyncExecutor() {
