1623cc5c8e812b4651c0fb276330c85a35e9375a
hwangdaesun <hds650588@gmail.com>
2024-07-07 20:27:40 +0900

    refactor/#360 : 사용하지 않는 클래스 제거

--- BE/exceed/src/test/java/com/gaebaljip/exceed/member/adapter/in/CreateGuestTestRequest.java
+++
@@ -1,4 +0,0 @@
-package com.gaebaljip.exceed.member.adapter.in;
-
-public record CreateGuestTestRequest(
-        Integer height, Integer gender, Integer weight, Integer age, String activity, String etc) {}
