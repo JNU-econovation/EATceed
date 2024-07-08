72fba768ec2de3c656f2f2c224b75dcffb7a93e3
hwangdaesun <hds650588@gmail.com>
2024-07-09 00:33:37 +0900

    refactor/#363 : @Profile 제거

--- BE/exceed/src/main/java/com/gaebaljip/exceed/config/S3Config.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/config/S3Config.java
@@ -3,14 +3,12 @@ package com.gaebaljip.exceed.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import software.amazon.awssdk.auth.credentials.AwsCredentials;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 
 @Configuration
-@Profile("!test")
 public class S3Config {
 
     @Value("${cloud.aws.credentials.access-key}")
--- BE/exceed/src/main/java/com/gaebaljip/exceed/config/SesConfig.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/config/SesConfig.java
@@ -3,14 +3,12 @@ package com.gaebaljip.exceed.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
 
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ses.SesAsyncClient;
 
 @Configuration
-@Profile("!test")
 public class SesConfig {
 
     @Value("${cloud.aws.credentials.access-key}")
