44fc422002629f6784bf10763e7185016a324274
Jin Hyuk <ekrrdj07@gmail.com>
2024-07-10 23:31:20 +0900

    test/#359: EmitterControllerTest

---
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/infrastructure/adapter/in/EmitterControllerTest.java
@@ -0,0 +1,38 @@
+package com.gaebaljip.exceed.infrastructure.adapter.in;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+
+import com.gaebaljip.exceed.common.ControllerTest;
+import com.gaebaljip.exceed.common.WithMockUser;
+import com.gaebaljip.exceed.infrastructure.sse.adapter.in.EmitterController;
+import com.gaebaljip.exceed.infrastructure.sse.application.port.in.ConnectEmitterUseCase;
+
+@WebMvcTest(EmitterController.class)
+public class EmitterControllerTest extends ControllerTest {
+
+    @MockBean private ConnectEmitterUseCase connectEmitterUseCase;
+
+    @Test
+    @WithMockUser(memberId = 1L)
+    void when_connect_expected_success() throws Exception {
+
+        // given
+        given(connectEmitterUseCase.execute(any(), any())).willReturn(new SseEmitter());
+
+        // when
+        ResultActions resultActions =
+                mockMvc.perform(get("/api/emitter/connect").contentType("text/event-stream"));
+
+        // then
+        resultActions.andExpect(status().isOk());
+    }
+}
