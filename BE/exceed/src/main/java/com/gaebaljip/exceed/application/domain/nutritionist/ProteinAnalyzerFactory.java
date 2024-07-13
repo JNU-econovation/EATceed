8f1e9e8c87074908425865b10c22f0cdd3cfee2a
hwangdaesun <hds650588@gmail.com>
2024-07-13 11:57:55 +0900

    refactor/#368 : 팩토리 메서드 패턴을 사용하여 각 분석 도메인을 생성하는 팩토리 클래스 생성

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/application/domain/nutritionist/ProteinAnalyzerFactory.java
@@ -0,0 +1,22 @@
+package com.gaebaljip.exceed.application.domain.nutritionist;
+
+import com.gaebaljip.exceed.application.domain.meal.DailyMeal;
+import com.gaebaljip.exceed.application.domain.member.Member;
+
+public class ProteinAnalyzerFactory extends AbstractAnalyzerFactory {
+
+    private ProteinAnalyzerFactory() {
+    }
+
+    private static class SingletonHolder{
+        private static final ProteinAnalyzerFactory INSTANCE = new ProteinAnalyzerFactory();
+    }
+
+    public static ProteinAnalyzerFactory getInstance() {
+        return SingletonHolder.INSTANCE;
+    }
+    @Override
+    public ProteinAnalyzer createAnalyzer(DailyMeal dailyMeal, Member member) {
+        return new ProteinAnalyzer(dailyMeal, member);
+    }
+}
