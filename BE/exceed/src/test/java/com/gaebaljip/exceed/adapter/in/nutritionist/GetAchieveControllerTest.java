e618bae2de9854d3dfc9d14ff6a2d0b0f72774ce
hwangdaesun <hds650588@gmail.com>
2024-07-20 12:39:08 +0900

    test/#414 : 분석 조회 웹 계층 단위 테스트 리팩토링

--- BE/exceed/src/test/java/com/gaebaljip/exceed/adapter/in/nutritionist/GetAchieveControllerTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/adapter/in/nutritionist/GetAchieveControllerTest.java
@@ -3,6 +3,9 @@ package com.gaebaljip.exceed.adapter.in.nutritionist;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -19,11 +22,14 @@ class GetAchieveControllerTest extends ControllerTest {
     @MockBean private GetCalorieAnalysisUsecase getAchieveUsecase;
 
     @Test
+    @DisplayName("분석 조회 성공" + "요청 시 날짜 포맷 확인")
     @WithMockUser
-    void getAchieve() throws Exception {
+    void when_getAnalysis_expected_success() throws Exception {
+
+        LocalDate date = LocalDate.of(2023, 12, 8);
+
         ResultActions resultActions =
-                mockMvc.perform(
-                        get("/v1/achieve/2023-12-08").contentType(MediaType.APPLICATION_JSON));
+                mockMvc.perform(get("/v1/achieve/" + date).contentType(MediaType.APPLICATION_JSON));
 
         resultActions.andExpect(status().isOk());
     }
