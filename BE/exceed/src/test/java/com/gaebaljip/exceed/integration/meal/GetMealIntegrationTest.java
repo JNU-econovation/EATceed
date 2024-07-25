33dc5f59421f4bc8d9496046eab51c6584d90f8a
hwangdaesun <hds650588@gmail.com>
2024-07-26 03:04:58 +0900

    test/#443 : 회원가입 후 하루이상이 지나 온보딩하는 회원의 경우 온보딩한 날짜에 켈린더 상세 조회하는 경우 테스트 추가

--- BE/exceed/src/test/java/com/gaebaljip/exceed/integration/meal/GetMealIntegrationTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/integration/meal/GetMealIntegrationTest.java
@@ -15,14 +15,18 @@ import java.util.List;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.restdocs.payload.JsonFieldType;
+import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.web.servlet.ResultActions;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.gaebaljip.exceed.adapter.in.meal.response.GetMealFoodResponse;
 import com.gaebaljip.exceed.adapter.in.meal.response.GetMealResponse;
+import com.gaebaljip.exceed.adapter.out.jpa.member.MemberRepository;
+import com.gaebaljip.exceed.application.domain.member.MemberEntity;
 import com.gaebaljip.exceed.common.ApiResponse;
 import com.gaebaljip.exceed.common.InitializeS3Bucket;
 import com.gaebaljip.exceed.common.IntegrationTest;
@@ -32,6 +36,8 @@ import com.gaebaljip.exceed.common.dto.MealRecordDTO;
 
 @InitializeS3Bucket
 public class GetMealIntegrationTest extends IntegrationTest {
+
+    @Autowired private MemberRepository memberRepository;
 
     @Test
     @DisplayName("성공 : 오늘 먹은 식사 조회")
@@ -166,7 +172,7 @@ public class GetMealIntegrationTest extends IntegrationTest {
     @WithMockUser
     void when_getSpecificMeal_expected_isVisited_false_mealRecord_existed() throws Exception {
 
-        LocalDate testData = LocalDate.of(2023, 12, 05);
+        LocalDate testData = LocalDate.of(2025, 12, 05);
 
         // when
         ResultActions resultActions =
@@ -194,9 +200,10 @@ public class GetMealIntegrationTest extends IntegrationTest {
     @Test
     @DisplayName("실패 : 2023년 11월 1일 기준 식사 조회" + "회원가입 전이기 때문에 오류 발생")
     @WithMockUser
-    void when_getSpecificMeal_expected_InValidDateFoundException() throws Exception {
+    void when_getSpecificMeal_createdAt_expected_InValidDateFoundException() throws Exception {
 
-        LocalDate testData = LocalDate.of(2023, 11, 01);
+        MemberEntity memberEntity = memberRepository.findById(1L).get();
+        LocalDate testData = memberEntity.getCreatedDate().toLocalDate();
 
         // when
         ResultActions resultActions =
@@ -205,5 +212,23 @@ public class GetMealIntegrationTest extends IntegrationTest {
                                 .contentType(MediaType.APPLICATION_JSON));
 
         resultActions.andExpectAll(status().isBadRequest(), jsonPath("$.error.code").value(6321));
+    }
+
+    @Test
+    @DisplayName("실패 : 회원가입 후 2일 후에 온보딩할 경우" + "예외 발생")
+    @Sql("classpath:db/testData_signup_after_2days_onboarding.sql")
+    @WithMockUser(memberId = 1L)
+    void when_signUp_onBoarding_after_2_days_user_getSpecificMea_updatedAt() throws Exception {
+        // given
+        MemberEntity memberEntity = memberRepository.findById(1L).get();
+        LocalDate testData = memberEntity.getUpdatedDate().toLocalDate();
+
+        ResultActions resultActions =
+                mockMvc.perform(
+                        RestDocumentationRequestBuilders.get("/v1/meal/" + testData)
+                                .contentType(MediaType.APPLICATION_JSON));
+
+        resultActions.andExpectAll(
+                status().is5xxServerError(), jsonPath("$.error.code").value(5555));
     }
 }
