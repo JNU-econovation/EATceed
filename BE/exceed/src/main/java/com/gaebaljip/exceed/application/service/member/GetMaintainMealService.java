cb2015ac89dc9150976e777a8e360d1018be9c32
hwangdaesun <hds650588@gmail.com>
2024-07-26 03:02:06 +0900

    refactor/#443 : null일 경우 NotFoundHistoryException 발생하도록 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/application/service/member/GetMaintainMealService.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/application/service/member/GetMaintainMealService.java
@@ -13,6 +13,7 @@ import com.gaebaljip.exceed.application.port.in.member.GetMaintainMealUsecase;
 import com.gaebaljip.exceed.application.port.out.member.HistoryPort;
 import com.gaebaljip.exceed.application.port.out.member.MemberPort;
 import com.gaebaljip.exceed.common.dto.MaintainMealDTO;
+import com.gaebaljip.exceed.common.exception.member.NotFoundHistoryException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -53,7 +54,9 @@ public class GetMaintainMealService implements GetMaintainMealUsecase {
             return toMaintainMeal(member);
         } else {
             HistoryEntity lastestHistoryEntity =
-                    historyPort.findMostRecentFutureMember(memberId, date);
+                    historyPort
+                            .findMostRecentFutureMember(memberId, date)
+                            .orElseThrow(() -> NotFoundHistoryException.EXECPTION);
             Member member = memberConverter.toModel(lastestHistoryEntity);
             return toMaintainMeal(member);
         }
