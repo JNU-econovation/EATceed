e5e0f94f0d2b43b5b575f2e314cc85228104e7ca
hwangdaesun <hds650588@gmail.com>
2024-07-26 02:59:46 +0900

    fix/#443 : 리턴값은 Optional로 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/application/port/out/member/HistoryPort.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/application/port/out/member/HistoryPort.java
@@ -2,6 +2,7 @@ package com.gaebaljip.exceed.application.port.out.member;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
 
 import com.gaebaljip.exceed.application.domain.member.HistoryEntity;
 import com.gaebaljip.exceed.application.domain.member.MemberEntity;
@@ -11,7 +12,7 @@ import com.gaebaljip.exceed.common.annotation.Port;
 public interface HistoryPort {
     HistoryEntity command(HistoryEntity historyEntity);
 
-    HistoryEntity findMostRecentFutureMember(Long memberId, LocalDateTime date);
+    Optional<HistoryEntity> findMostRecentFutureMember(Long memberId, LocalDateTime date);
 
     List<HistoryEntity> findByMemberEntity(MemberEntity memberEntity);
 
