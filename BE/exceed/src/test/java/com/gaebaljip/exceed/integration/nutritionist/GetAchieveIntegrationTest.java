152a36311b715be19a17368ac79e3447d915ad7a
hwangdaesun <hds650588@gmail.com>
2024-08-01 14:17:54 +0900

    test/#446 : 회원가입 하기 이전의 달의 기록을 못 보는 경우 고려하여 통합 테스트 추가 및 기존 테스트 날짜 수정

--- BE/exceed/src/test/java/com/gaebaljip/exceed/integration/nutritionist/GetAchieveIntegrationTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/integration/nutritionist/GetAchieveIntegrationTest.java
@@ -3,11 +3,13 @@ package com.gaebaljip.exceed.integration.nutritionist;
 import static com.gaebaljip.exceed.common.util.ApiDocumentUtil.getDocumentRequest;
 import static com.gaebaljip.exceed.common.util.ApiDocumentUtil.getDocumentResponse;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
 
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
@@ -19,14 +21,16 @@ import com.gaebaljip.exceed.adapter.in.nutritionist.response.GetCalorieAnalysisR
 import com.gaebaljip.exceed.common.ApiResponse;
 import com.gaebaljip.exceed.common.IntegrationTest;
 import com.gaebaljip.exceed.common.WithMockUser;
+import com.gaebaljip.exceed.common.exception.meal.MealError;
 
 public class GetAchieveIntegrationTest extends IntegrationTest {
     @Test
+    @DisplayName("2023-12-01 08:00:00에 회원가입한 회원이 2023-12월의 월별 달성률 볼 경우 성공")
     @Transactional
     @WithMockUser
-    void getAchieves() throws Exception {
+    void when_getAchieves_success() throws Exception {
         // given
-        LocalDate testData = LocalDate.of(2024, 6, 6);
+        LocalDate testData = LocalDate.of(2023, 12, 6);
 
         // when
         ResultActions resultActions =
@@ -55,5 +59,24 @@ public class GetAchieveIntegrationTest extends IntegrationTest {
                                 "get-achieves-success",
                                 getDocumentRequest(),
                                 getDocumentResponse()));
+    }
+
+    @Test
+    @DisplayName("2023-12-01 08:00:00에 회원가입한 회원이 2023-11월의 월별 달성률을 보려고할 때 예외 발생")
+    @Transactional
+    @WithMockUser
+    void when_getAchieves_fail() throws Exception {
+        // given
+        LocalDate testData = LocalDate.of(2023, 11, 6);
+
+        // when
+        ResultActions resultActions =
+                mockMvc.perform(
+                        RestDocumentationRequestBuilders.get("/v1/achieve/" + testData)
+                                .contentType(MediaType.APPLICATION_JSON));
+        // then
+        resultActions.andExpectAll(
+                status().isBadRequest(),
+                jsonPath("$.error.reason").value(MealError.INVALID_DATE_FOUND.getReason()));
     }
 }
