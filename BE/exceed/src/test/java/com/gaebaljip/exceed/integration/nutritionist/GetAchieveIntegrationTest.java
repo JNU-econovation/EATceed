8b3e66fb9774de4f5ceb6448721e28dd8c288a2c
hwangdaesun <hds650588@gmail.com>
2024-08-03 20:37:09 +0900

    test/#446 : 분석 API 날짜를 고려하여 통합 테스트 개선 및 테스트 추가

--- BE/exceed/src/test/java/com/gaebaljip/exceed/integration/nutritionist/GetAchieveIntegrationTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/integration/nutritionist/GetAchieveIntegrationTest.java
@@ -1,34 +1,36 @@
 package com.gaebaljip.exceed.integration.nutritionist;
 
-import static com.gaebaljip.exceed.common.util.ApiDocumentUtil.getDocumentRequest;
-import static com.gaebaljip.exceed.common.util.ApiDocumentUtil.getDocumentResponse;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import java.time.LocalDate;
+import java.util.List;
+import java.util.stream.Collectors;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 import org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.gaebaljip.exceed.adapter.in.nutritionist.response.GetCalorieAnalysisResponse;
 import com.gaebaljip.exceed.common.ApiResponse;
 import com.gaebaljip.exceed.common.IntegrationTest;
 import com.gaebaljip.exceed.common.WithMockUser;
+import com.gaebaljip.exceed.common.dto.CalorieAnalysisDTO;
 import com.gaebaljip.exceed.common.exception.meal.MealError;
 
 public class GetAchieveIntegrationTest extends IntegrationTest {
     @Test
-    @DisplayName("2023-12-01 08:00:00에 회원가입한 회원이 2023-12월의 월별 달성률 볼 경우 성공")
-    @Transactional
+    @DisplayName(
+            "2023-12-01 08:00:00에 회원가입한 회원이 2023-12월의 월별 달성률 볼 경우"
+                    + "모든 isVisited는 false"
+                    + "모든 isCalorieAchieved는 false")
     @WithMockUser
-    void when_getAchieves_success() throws Exception {
+    void when_getAchieves_expected_allFalse() throws Exception {
         // given
         LocalDate testData = LocalDate.of(2023, 12, 6);
 
@@ -47,23 +49,77 @@ public class GetAchieveIntegrationTest extends IntegrationTest {
                         new TypeReference<ApiResponse.CustomBody<GetCalorieAnalysisResponse>>() {});
 
         int comparedSize =
-                getAchieveListResponseCustomBody.getResponse().calorieAnalysisDTOS().stream()
-                        .toList()
-                        .size();
+                getAnalysisDTOS(getAchieveListResponseCustomBody).stream().toList().size();
 
-        Assertions.assertThat(comparedSize).isEqualTo(testData.lengthOfMonth());
-        resultActions
-                .andExpect(status().isOk())
-                .andDo(
-                        document(
-                                "get-achieves-success",
-                                getDocumentRequest(),
-                                getDocumentResponse()));
+        List<Boolean> isCalorieAchievedList =
+                getAnalysisDTOS(getAchieveListResponseCustomBody).stream()
+                        .map(calorieAnalysisDTO -> calorieAnalysisDTO.isCalorieAchieved())
+                        .toList();
+
+        List<Boolean> isVisitedList =
+                getAnalysisDTOS(getAchieveListResponseCustomBody).stream()
+                        .map(calorieAnalysisDTO -> calorieAnalysisDTO.isVisited())
+                        .toList();
+
+        assertAll(
+                () -> assertTrue(comparedSize == testData.lengthOfMonth()),
+                () ->
+                        assertTrue(
+                                isCalorieAchievedList.stream().collect(Collectors.toSet()).size()
+                                                == 1
+                                        && isCalorieAchievedList.get(0) == false),
+                () ->
+                        assertTrue(
+                                isVisitedList.stream().collect(Collectors.toSet()).size() == 1
+                                        && isVisitedList.get(0) == false));
+        resultActions.andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("2023-12-01 08:00:00에 회원가입한 회원이 2024-6월의 월별 달성률 볼 경우")
+    @WithMockUser
+    void when_getAchieves_success() throws Exception {
+        // given
+        LocalDate testData = LocalDate.of(2024, 06, 6);
+
+        // when
+        ResultActions resultActions =
+                mockMvc.perform(
+                        RestDocumentationRequestBuilders.get("/v1/achieve/" + testData)
+                                .contentType(MediaType.APPLICATION_JSON));
+        // then
+
+        String responseBody = resultActions.andReturn().getResponse().getContentAsString();
+
+        ApiResponse.CustomBody<GetCalorieAnalysisResponse> getAchieveListResponseCustomBody =
+                om.readValue(
+                        responseBody,
+                        new TypeReference<ApiResponse.CustomBody<GetCalorieAnalysisResponse>>() {});
+
+        int comparedSize =
+                getAnalysisDTOS(getAchieveListResponseCustomBody).stream().toList().size();
+
+        List<Boolean> isCalorieAchievedList =
+                getAnalysisDTOS(getAchieveListResponseCustomBody).stream()
+                        .map(calorieAnalysisDTO -> calorieAnalysisDTO.isCalorieAchieved())
+                        .toList();
+
+        List<Boolean> isVisitedList =
+                getAnalysisDTOS(getAchieveListResponseCustomBody).stream()
+                        .map(calorieAnalysisDTO -> calorieAnalysisDTO.isVisited())
+                        .toList();
+
+        assertAll(() -> assertTrue(comparedSize == testData.lengthOfMonth()));
+        resultActions.andExpect(status().isOk());
+    }
+
+    private List<CalorieAnalysisDTO> getAnalysisDTOS(
+            ApiResponse.CustomBody<GetCalorieAnalysisResponse> getAchieveListResponseCustomBody) {
+        return getAchieveListResponseCustomBody.getResponse().calorieAnalysisDTOS();
     }
 
     @Test
     @DisplayName("2023-11-01 08:00:00에 회원가입한 회원이 2023-10월의 월별 달성률을 보려고할 때 예외 발생")
-    @Transactional
     @WithMockUser
     void when_getAchieves_fail() throws Exception {
         // given
