d90964a717e8b4180b3449ed46ca5c41a41f026e
hwangdaesun <hds650588@gmail.com>
2024-08-03 20:27:16 +0900

    feat/#446 :  한달치 먹은 식사를 나타내는 일급 컬렉션 생성

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/exception/nutritionist/MonthDaysSizeMismatchException.java
@@ -0,0 +1,13 @@
+package com.gaebaljip.exceed.common.exception.nutritionist;
+
+import com.gaebaljip.exceed.common.exception.EatCeedException;
+import lombok.Getter;
+
+@Getter
+public class MonthDaysSizeMismatchException extends EatCeedException {
+
+    public static EatCeedException EXECPTION = new MonthDaysSizeMismatchException();
+    private MonthDaysSizeMismatchException() {
+        super(NutritionistError.NOT_SAME_SIZE);
+    }
+}
---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/exception/nutritionist/SameYearAndMonthException.java
@@ -0,0 +1,14 @@
+package com.gaebaljip.exceed.common.exception.nutritionist;
+
+import com.gaebaljip.exceed.common.exception.EatCeedException;
+import lombok.Getter;
+
+@Getter
+public class SameYearAndMonthException extends EatCeedException {
+
+    public static EatCeedException EXECPTION = new SameYearAndMonthException();
+    private SameYearAndMonthException(){
+        super(NutritionistError.NOT_SAME_YEAR_MONTH);
+    }
+
+}
