cc97c385b72da827ab4372df22d603f621b86188
hwangdaesun <hds650588@gmail.com>
2024-08-03 20:36:05 +0900

    refactor/#446 : @Transactional(readOnly = true) 추가

--- BE/exceed/src/main/java/com/gaebaljip/exceed/application/service/meal/ValidateBeforeSignupDateService.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/application/service/meal/ValidateBeforeSignupDateService.java
@@ -3,6 +3,7 @@ package com.gaebaljip.exceed.application.service.meal;
 import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.gaebaljip.exceed.adapter.out.jpa.member.MemberPersistenceAdapter;
 import com.gaebaljip.exceed.application.domain.member.MemberEntity;
@@ -18,6 +19,7 @@ public class ValidateBeforeSignupDateService implements ValidateBeforeSignUpDate
     private final MemberPersistenceAdapter memberPersistenceAdapter;
 
     @Override
+    @Transactional(readOnly = true)
     public void execute(Long memberId, LocalDateTime dateTime) {
         MemberEntity memberEntity = memberPersistenceAdapter.query(memberId);
         if (memberEntity.checkIfBeforeSignUpDate(dateTime, memberEntity.getCreatedDate())) {
