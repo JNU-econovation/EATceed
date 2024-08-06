cdf7086fca07f9a42c00666f6d2a6ecb7ef9f8d2
hwangdaesun <hds650588@gmail.com>
2024-08-06 21:31:26 +0900

    test/#451 : MealPersistenceAdapter query 메서드 2개 테스트

--- BE/exceed/src/test/java/com/gaebaljip/exceed/common/DatabaseTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/common/DatabaseTest.java
@@ -5,12 +5,26 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 
+import com.gaebaljip.exceed.adapter.out.jpa.food.FoodConverter;
+import com.gaebaljip.exceed.adapter.out.jpa.meal.ConsumedFoodConverter;
+import com.gaebaljip.exceed.adapter.out.jpa.meal.MealConverter;
+import com.gaebaljip.exceed.adapter.out.jpa.meal.MealFoodConverter;
+import com.gaebaljip.exceed.adapter.out.jpa.meal.MealPersistenceAdapter;
 import com.gaebaljip.exceed.adapter.out.jpa.member.HistoryPersistenceAdapter;
 import com.gaebaljip.exceed.application.service.member.MemberConverter;
 import com.gaebaljip.exceed.config.QueryDslConfig;
 
 @Sql("classpath:db/testData.sql")
-@Import({QueryDslConfig.class, MemberConverter.class, HistoryPersistenceAdapter.class})
+@Import({
+    QueryDslConfig.class,
+    MemberConverter.class,
+    HistoryPersistenceAdapter.class,
+    MealPersistenceAdapter.class,
+    MealConverter.class,
+    FoodConverter.class,
+    ConsumedFoodConverter.class,
+    MealFoodConverter.class
+})
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public abstract class DatabaseTest extends ContainerTest {}
