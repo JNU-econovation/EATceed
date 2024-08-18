37f526c9c9ef9023df9385905f110d8d561d61d1
hwangdaesun <hds650588@gmail.com>
2024-08-18 20:54:00 +0900

    test/#470 : 사용하지 않는 클래스 제거로 인한 변경

--- BE/exceed/src/test/java/com/gaebaljip/exceed/common/DatabaseTest.java
+++ BE/exceed/src/test/java/com/gaebaljip/exceed/common/DatabaseTest.java
@@ -5,10 +5,6 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.jdbc.Sql;
 
-import com.gaebaljip.exceed.adapter.out.jpa.food.FoodConverter;
-import com.gaebaljip.exceed.adapter.out.jpa.meal.ConsumedFoodConverter;
-import com.gaebaljip.exceed.adapter.out.jpa.meal.MealConverter;
-import com.gaebaljip.exceed.adapter.out.jpa.meal.MealFoodConverter;
 import com.gaebaljip.exceed.adapter.out.jpa.meal.MealPersistenceAdapter;
 import com.gaebaljip.exceed.adapter.out.jpa.member.HistoryPersistenceAdapter;
 import com.gaebaljip.exceed.application.service.member.MemberConverter;
@@ -19,11 +15,7 @@ import com.gaebaljip.exceed.config.QueryDslConfig;
     QueryDslConfig.class,
     MemberConverter.class,
     HistoryPersistenceAdapter.class,
-    MealPersistenceAdapter.class,
-    MealConverter.class,
-    FoodConverter.class,
-    ConsumedFoodConverter.class,
-    MealFoodConverter.class
+    MealPersistenceAdapter.class
 })
 @DataJpaTest
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
