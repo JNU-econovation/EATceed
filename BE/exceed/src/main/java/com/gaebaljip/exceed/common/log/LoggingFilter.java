5888edc566b854b697ce1ede87241a1c4c5fc4d3
hwangdaesun <hds650588@gmail.com>
2024-09-01 19:19:34 +0900

    refactor/#477 : HttpLogMessage 클래스를 도입하여 LoggingFilter 리팩토링

--- BE/exceed/src/main/java/com/gaebaljip/exceed/common/log/LoggingFilter.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/log/LoggingFilter.java
@@ -10,6 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.slf4j.MDC;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StopWatch;
 import org.springframework.web.filter.OncePerRequestFilter;
 import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
@@ -35,26 +36,23 @@ public class LoggingFilter extends OncePerRequestFilter {
             filterChain.doFilter(request, response);
             return;
         }
-        MDC.put("traceId", UUID.randomUUID().toString());
+        MDC.put(LogKey.KEY, UUID.randomUUID().toString());
 
-        ContentCachingRequestWrapper httpServletRequest =
-                new ContentCachingRequestWrapper((HttpServletRequest) request);
+        ContentCachingRequestWrapper httpServletRequest = new ContentCachingRequestWrapper(request);
         ContentCachingResponseWrapper httpServletResponse =
-                new ContentCachingResponseWrapper((HttpServletResponse) response);
-        // 이렇게 한다고 해도 Byte의 길이만 설정해 두고 컨탠츠들은 복사하지 않는다 그래서 doFilter 다음에 실행시킨다.
-
+                new ContentCachingResponseWrapper(response);
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
         filterChain.doFilter(httpServletRequest, httpServletResponse);
+        stopWatch.stop();
 
-        String url = httpServletRequest.getRequestURI();
-        String reqContent = new String(httpServletRequest.getContentAsByteArray());
-        log.info("request url : {}, request body : {}", url, reqContent);
+        HttpLogMessage httpLogMessage =
+                HttpLogMessage.of(
+                        httpServletRequest, httpServletResponse, stopWatch.getTotalTimeSeconds());
+        log.info("{}", httpLogMessage);
 
-        String resContent = new String(httpServletResponse.getContentAsByteArray());
-        int httpStatus = httpServletResponse.getStatus();
-        // Response도 마찬가지로 한번 읽으면 사라진다. 그래서 다시 채워줘야한다.ㅉ
         httpServletResponse.copyBodyToResponse(); // 다시한번 더 바디를 채워준다.
 
-        log.info("response status : {}, responseBody : {}", httpStatus, resContent);
-        MDC.clear();
+        MDC.remove(LogKey.KEY);
     }
 }
