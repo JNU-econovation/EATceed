58184549e8bfff34461cad7324bf2b6d66b9565c
hwangdaesun <hds650588@gmail.com>
2024-09-06 00:31:17 +0900

    feat/#480 : Gmail에서 딥링크가 직접적으로 사용안되는 이슈로 리다이렉트해서 딥링크를 전송하는 API 구현

--- BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/CheckMemberEmailController.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/CheckMemberEmailController.java
@@ -1,15 +1,15 @@
 package com.gaebaljip.exceed.adapter.in.member;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.gaebaljip.exceed.adapter.in.member.request.CheckMemberRequest;
 import com.gaebaljip.exceed.application.port.in.member.CheckCodeUsecase;
+import com.gaebaljip.exceed.application.port.in.member.GetCodeUsecase;
 import com.gaebaljip.exceed.common.ApiResponse;
 import com.gaebaljip.exceed.common.ApiResponseGenerator;
 
@@ -25,11 +25,24 @@ import lombok.RequiredArgsConstructor;
 public class CheckMemberEmailController {
 
     private final CheckCodeUsecase checkCodeUsecase;
+    private final GetCodeUsecase getCodeUsecase;
+
+    @Value("${exceed.deepLink}")
+    private String deepLink;
 
     @GetMapping("/members/checked")
     public ApiResponse<ApiResponse.CustomBody<Void>> checkMemberEmail(
             @ModelAttribute @Valid CheckMemberRequest checkMemberRequest) {
         checkCodeUsecase.execute(checkMemberRequest);
         return ApiResponseGenerator.success(HttpStatus.OK);
+    }
+
+    @GetMapping("/email/redirect")
+    public void redirect(@RequestParam String email, HttpServletResponse response) {
+        StringBuilder sb = new StringBuilder();
+        String code = getCodeUsecase.execute(email);
+        String redirectUrl = sb.append(deepLink).append("?code=").append(code).toString();
+        response.setHeader("Location", redirectUrl);
+        response.setStatus(302);
     }
 }
