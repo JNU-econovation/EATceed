cd0e9e21ed87df3f23fe91da1d7d871b84d41869
hwangdaesun <hds650588@gmail.com>
2024-09-11 01:52:17 +0900

    feat/#488 : 속도 향상을 위해 IpvParameterSpec 제거

--- BE/exceed/src/main/java/com/gaebaljip/exceed/common/Encryption.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/Encryption.java
@@ -3,7 +3,6 @@ package com.gaebaljip.exceed.common;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
 import java.util.Arrays;
 import java.util.Base64;
 import java.util.Objects;
@@ -11,7 +10,6 @@ import java.util.Objects;
 import javax.annotation.PostConstruct;
 import javax.crypto.Cipher;
 import javax.crypto.NoSuchPaddingException;
-import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
 import org.springframework.beans.factory.annotation.Value;
@@ -35,13 +33,12 @@ public class Encryption {
     private String secret;
 
     private SecretKeySpec secretKeySpec;
-    private IvParameterSpec ivParameterSpec;
     private Cipher cipher;
 
     @Timer
     public String encrypt(String value) {
         try {
-            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec, ivParameterSpec);
+            cipher.init(Cipher.ENCRYPT_MODE, secretKeySpec);
             byte[] valueBytes = value.getBytes(StandardCharsets.UTF_8); // String을 바이트 배열로 변환
             byte[] encrypted = cipher.doFinal(valueBytes);
             return Base64.getUrlEncoder().encodeToString(encrypted);
@@ -53,7 +50,7 @@ public class Encryption {
     @Timer
     public String decrypt(final String encryptedValue) {
         try {
-            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, ivParameterSpec);
+            cipher.init(Cipher.DECRYPT_MODE, secretKeySpec);
             byte[] encryptedBytes = Base64.getUrlDecoder().decode(encryptedValue);
             byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
             return new String(decryptedBytes, StandardCharsets.UTF_8); // 바이트 배열을 String으로 변환
@@ -71,17 +68,11 @@ public class Encryption {
 
     @PostConstruct
     public void init() throws NoSuchPaddingException, NoSuchAlgorithmException {
-        SecureRandom secureRandom = new SecureRandom();
-        byte[] iv = new byte[16]; // 16bytes = 128bits
-        secureRandom.nextBytes(iv);
-        ivParameterSpec = new IvParameterSpec(iv);
-
         byte[] keyBytes = secret.getBytes(StandardCharsets.UTF_8);
         MessageDigest sha = MessageDigest.getInstance("SHA-256");
         keyBytes = sha.digest(keyBytes);
         keyBytes = Arrays.copyOf(keyBytes, 16); // AES-128을 위한 16바이트 키
         secretKeySpec = new SecretKeySpec(keyBytes, algorithm);
-
         cipher = Cipher.getInstance(encryptionSpec);
     }
 }
