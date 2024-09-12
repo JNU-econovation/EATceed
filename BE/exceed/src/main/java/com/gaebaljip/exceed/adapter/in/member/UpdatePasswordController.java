0b05fb0be0b1f069c943b73f29ec1f73059c5a0b
hwangdaesun <hds650588@gmail.com>
2024-09-13 02:48:55 +0900

    feat/#491 : 비밀번호 변경 API 구현

--- BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/UpdatePasswordController.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/UpdatePasswordController.java
@@ -3,7 +3,10 @@ package com.gaebaljip.exceed.adapter.in.member;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import com.gaebaljip.exceed.adapter.in.member.request.UpdatePasswordRequest;
+import com.gaebaljip.exceed.application.port.in.member.CheckCodeUsecase;
 import com.gaebaljip.exceed.application.port.in.member.GetCodeUsecase;
+import com.gaebaljip.exceed.application.port.in.member.UpdatePasswordUsecase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -28,6 +31,9 @@ public class UpdatePasswordController {
     private final GetCodeUsecase getCodeUsecase;
     @Value("${exceed.deepLink.updatePassword}")
     private String deepLink;
+    private final CheckCodeUsecase checkCodeUsecase;
+    private final UpdatePasswordUsecase updatePasswordUsecase;
+
     @Operation(
             summary = "비밀번호 변경 전 이메일 검증 및 메일 전송",
             description = "비밀번호 변경하기 전, 이메일 검증 및 이메일을 재전송한다.")
@@ -37,6 +43,7 @@ public class UpdatePasswordController {
         passwordValidationUsecase.execute(request.email());
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
+
     @Operation(summary = "링크 클릭시 리다이렉트", description = "AOS는 몰라도 되는 API")
     @GetMapping("/updatePassword-redirect")
     public void redirect(@RequestParam String email, HttpServletResponse response) {
@@ -45,6 +52,17 @@ public class UpdatePasswordController {
         String redirectUrl = sb.append(deepLink).append("?code=").append(code).toString();
         response.setHeader("Location", redirectUrl);
         response.setStatus(302);
+    }
+    @Operation(
+            summary = "비밀번호 찾기 및 변경",
+            description = "비밀번호 변경시 사용하고, 비밀번호를 찾을 시에도 무조건 비밀번호를 변경한다.")
+    @PatchMapping("/members/password")
+    public ApiResponse<CustomBody<Void>> updatePassword(
+            @RequestBody @Valid UpdatePasswordRequest request) {
+        passwordValidationUsecase.execute(request.email());
+        checkCodeUsecase.execute(request.email(), request.code());
+        updatePasswordUsecase.execute(request.email(), request.newPassword());
+        return ApiResponseGenerator.success(HttpStatus.OK);
     }
 }
 
