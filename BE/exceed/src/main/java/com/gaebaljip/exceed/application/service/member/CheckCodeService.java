18e3e49e9077e5f117aff54fbc59adf17136222c
hwangdaesun <hds650588@gmail.com>
2024-09-13 02:35:00 +0900

    refactor/#491 : 기존 유스케이스에서 MemberEntity의 checked 컬럼을 수정하는 부분을 분리 후 이를 통해 새로운 유스케이스를 생성

--- BE/exceed/src/main/java/com/gaebaljip/exceed/application/service/member/CheckCodeService.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/application/service/member/CheckCodeService.java
@@ -3,11 +3,8 @@ package com.gaebaljip.exceed.application.service.member;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.gaebaljip.exceed.adapter.in.member.request.CheckMemberRequest;
-import com.gaebaljip.exceed.application.domain.member.MemberEntity;
 import com.gaebaljip.exceed.application.port.in.member.CheckCodeUsecase;
 import com.gaebaljip.exceed.application.port.out.member.CodePort;
-import com.gaebaljip.exceed.application.port.out.member.MemberPort;
 import com.gaebaljip.exceed.common.Encryption;
 import com.gaebaljip.exceed.common.annotation.Timer;
 import com.gaebaljip.exceed.common.exception.member.ExpiredCodeException;
@@ -20,20 +17,13 @@ public class CheckCodeService implements CheckCodeUsecase {
 
     private final Encryption encryption;
     private final CodePort codePort;
-    private final MemberPort memberPort;
 
     @Override
     @Transactional
     @Timer
-    public void execute(CheckMemberRequest checkMemberRequest) {
-        String code =
-                codePort.query(checkMemberRequest.email())
-                        .orElseThrow(() -> ExpiredCodeException.EXECPTION);
-        String decrypt = encryption.decrypt(checkMemberRequest.code());
-        if (encryption.match(decrypt, code)) {
-            MemberEntity member = memberPort.findMemberByEmail(checkMemberRequest.email());
-            member.updateChecked();
-            codePort.delete(checkMemberRequest.email());
-        }
+    public void execute(String email, String encrypt) {
+        String code = codePort.query(email).orElseThrow(() -> ExpiredCodeException.EXECPTION);
+        String decrypt = encryption.decrypt(encrypt);
+        encryption.match(decrypt, code);
     }
 }
