523c4c92ae93c7359184141faca3b540f02a8b67
hwangdaesun <hds650588@gmail.com>
2024-09-13 02:46:40 +0900

    feat/#491 : 비밀번호 변경 링크 클릭시 딥링크로 리다이렉트 되는 API 구현

--- BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/UpdatePasswordController.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/UpdatePasswordController.java
@@ -1,7 +1,10 @@
 package com.gaebaljip.exceed.adapter.in.member;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import com.gaebaljip.exceed.application.port.in.member.GetCodeUsecase;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -22,6 +25,9 @@ import lombok.RequiredArgsConstructor;
 public class UpdatePasswordController {
 
     private final PasswordValidationUsecase passwordValidationUsecase;
+    private final GetCodeUsecase getCodeUsecase;
+    @Value("${exceed.deepLink.updatePassword}")
+    private String deepLink;
     @Operation(
             summary = "비밀번호 변경 전 이메일 검증 및 메일 전송",
             description = "비밀번호 변경하기 전, 이메일 검증 및 이메일을 재전송한다.")
@@ -31,4 +37,14 @@ public class UpdatePasswordController {
         passwordValidationUsecase.execute(request.email());
         return ApiResponseGenerator.success(HttpStatus.OK);
     }
+    @Operation(summary = "링크 클릭시 리다이렉트", description = "AOS는 몰라도 되는 API")
+    @GetMapping("/updatePassword-redirect")
+    public void redirect(@RequestParam String email, HttpServletResponse response) {
+        StringBuilder sb = new StringBuilder();
+        String code = getCodeUsecase.execute(email);
+        String redirectUrl = sb.append(deepLink).append("?code=").append(code).toString();
+        response.setHeader("Location", redirectUrl);
+        response.setStatus(302);
+    }
 }
+
