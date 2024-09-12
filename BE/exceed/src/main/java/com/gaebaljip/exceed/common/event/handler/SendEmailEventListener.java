e4ea141520daee6b93a3a812bd268c04a8448f45
hwangdaesun <hds650588@gmail.com>
2024-09-13 02:52:07 +0900

    feat/#491 : 이메일을 보내는 리스너 구현 및 메일 관련 템플릿 수정

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/event/handler/SendEmailEventListener.java
@@ -0,0 +1,46 @@
+package com.gaebaljip.exceed.common.event.handler;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.event.TransactionalEventListener;
+import org.thymeleaf.context.Context;
+
+import com.gaebaljip.exceed.application.domain.member.Code;
+import com.gaebaljip.exceed.application.port.out.member.CodePort;
+import com.gaebaljip.exceed.application.port.out.member.EmailPort;
+import com.gaebaljip.exceed.common.MailTemplate;
+import com.gaebaljip.exceed.common.event.SendEmailEvent;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class SendEmailEventListener {
+    private final EmailPort emailPort;
+    private final CodePort codePort;
+
+    @Value("${exceed.url}")
+    private String URL;
+
+    private Long expiredTime = 600000L;
+
+    @TransactionalEventListener(classes = SendEmailEvent.class)
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Async
+    public void handle(SendEmailEvent event) {
+        codePort.saveWithExpiration(event.getEmail(), Code.create(), expiredTime);
+        Context context = new Context();
+        context.setVariable(
+                MailTemplate.UPDATE_PASSWORD_MAIL_CONTEXT,
+                URL + MailTemplate.REPLY_TO_UPDATE_PASSWORD_MAIL_URL);
+        context.setVariable(MailTemplate.UPDATE_PASSWORD_EMAIL, "?email=" + event.getEmail());
+        emailPort.sendEmail(
+                event.getEmail(),
+                MailTemplate.UPDATE_PASSWORD_TITLE,
+                MailTemplate.UPDATE_PASSWORD_TEMPLATE,
+                context);
+    }
+}
