e1ddbfb883a7be9ff8267af018988ed79bfce6d3
hwangdaesun <hds650588@gmail.com>
2024-09-13 11:04:36 +0900

    feat/#491 : 예외 케이스 Swagger에 반영

--- BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/UpdatePasswordController.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/UpdatePasswordController.java
@@ -7,6 +7,9 @@ import com.gaebaljip.exceed.adapter.in.member.request.UpdatePasswordRequest;
 import com.gaebaljip.exceed.application.port.in.member.CheckCodeUsecase;
 import com.gaebaljip.exceed.application.port.in.member.GetCodeUsecase;
 import com.gaebaljip.exceed.application.port.in.member.UpdatePasswordUsecase;
+import com.gaebaljip.exceed.common.docs.member.UpdatePassword_updatePasswordExceptionDocs;
+import com.gaebaljip.exceed.common.docs.member.UpdatePassword_validateEmailExceptionDocs;
+import com.gaebaljip.exceed.common.swagger.ApiErrorExceptionsExample;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -38,6 +41,7 @@ public class UpdatePasswordController {
             summary = "비밀번호 변경 전 이메일 검증 및 메일 전송",
             description = "비밀번호 변경하기 전, 이메일 검증 및 이메일을 재전송한다.")
     @PostMapping("/email")
+    @ApiErrorExceptionsExample(UpdatePassword_validateEmailExceptionDocs.class)
     public ApiResponse<CustomBody<Void>> validateEmail(
             @RequestBody @Valid SendEmailRequest request) {
         passwordValidationUsecase.execute(request.email());
@@ -57,6 +61,7 @@ public class UpdatePasswordController {
             summary = "비밀번호 찾기 및 변경",
             description = "비밀번호 변경시 사용하고, 비밀번호를 찾을 시에도 무조건 비밀번호를 변경한다.")
     @PatchMapping("/members/password")
+    @ApiErrorExceptionsExample(UpdatePassword_updatePasswordExceptionDocs.class)
     public ApiResponse<CustomBody<Void>> updatePassword(
             @RequestBody @Valid UpdatePasswordRequest request) {
         passwordValidationUsecase.execute(request.email());
