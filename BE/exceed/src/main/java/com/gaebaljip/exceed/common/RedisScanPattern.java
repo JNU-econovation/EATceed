bbf1382c5befd2e785288c8f93f38b1cfdefc574
hwangdaesun <hds650588@gmail.com>
2024-09-20 22:34:38 +0900

    feat/#505 : Redis scan의 match option 결정하는 RedisScanPattern 클래스 생성

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/RedisScanPattern.java
@@ -0,0 +1,11 @@
+package com.gaebaljip.exceed.common;
+
+import static com.gaebaljip.exceed.common.RedisKeys.ANALYSIS_CACHE_KEY;
+
+import java.time.LocalDate;
+
+public class RedisScanPattern {
+    public static String getAnalysisPattern(LocalDate now) {
+        return ANALYSIS_CACHE_KEY + "*_" + now.toString().substring(0, 8) + "*";
+    }
+}
