edf5ad7fc045fb5a613db20b92057e4a15f1d213
hwangdaesun <hds650588@gmail.com>
2024-09-20 22:37:30 +0900

    feat/#505 : 특정 회원의 특정 날짜의 칼로리 달성 여부 조회 결과를 사용해 해당 월의 분석 결과에 반영하는 DailyAnalysisCacheUpdater 클래스 생성

---
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/out/scheduler/quartz/DailyAnalysisCacheUpdater.java
@@ -0,0 +1,34 @@
+package com.gaebaljip.exceed.adapter.out.scheduler.quartz;
+
+import static com.gaebaljip.exceed.common.RedisKeys.ANALYSIS_CACHE_TTL;
+
+import org.springframework.stereotype.Component;
+
+import com.gaebaljip.exceed.adapter.in.nutritionist.response.GetMonthlyAnalysisResponse;
+import com.gaebaljip.exceed.adapter.out.redis.RedisUtils;
+import com.gaebaljip.exceed.common.dto.CalorieAnalysisDTO;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class DailyAnalysisCacheUpdater {
+
+    private final RedisUtils redisUtils;
+
+    /** 특정 회원의 특정 날짜의 칼로리 달성 여부 조회 결과(CalorieAnalysisDTO)를 사용해 해당 월의 분석 결과 update */
+    public void updateCacheForKey(String key, CalorieAnalysisDTO calorieAnalysisDTO) {
+        try {
+            log.info("cacheKeyProcessor 실행, key : {}", key);
+            GetMonthlyAnalysisResponse response =
+                    GetMonthlyAnalysisResponse.overWrite(
+                            GetMonthlyAnalysisResponse.read(redisUtils.getData(key)),
+                            calorieAnalysisDTO);
+            redisUtils.setData(key, GetMonthlyAnalysisResponse.write(response), ANALYSIS_CACHE_TTL);
+        } catch (Exception e) {
+            log.error("Error processing key: {}, message: {}", key, e.getMessage());
+        }
+    }
+}
