fbc5f1ac036b0a7042693d3eb34a3441b16f04f2
Park Kyeong Jun <100195725+Kyeong6@users.noreply.github.com>
2024-09-22 19:58:32 +0900

    feat: 토큰관련 커스텀 예외처리 추가(6개)

--- AI/server/errors/custom_exceptions.py
+++ AI/server/errors/custom_exceptions.py
@@ -17,3 +17,23 @@ class AnalysisError(Exception):
 class TokenError(Exception):
     def __init__(self, message: str):
         self.message: str = message
+
+class InvalidJWT(TokenError):
+    def __init__(self):
+        super().__init__("잘못된 토큰입니다.")
+
+class ExpiredJWT(TokenError):
+    def __init__(self):
+        super().__init__("만료된 토큰입니다.")
+
+class UnsupportedJWT(TokenError):
+    def __init__(self):
+        super().__init__("지원되지 않는 토큰입니다.")
+
+class SignatureJWT(TokenError):
+    def __init__(self):
+        super().__init__("토큰의 형식이 잘못 됐습니다.")
+
+class NeedAuthentication(TokenError):
+    def __init__(self):
+        super().__init__("인증이 필요합니다.")
