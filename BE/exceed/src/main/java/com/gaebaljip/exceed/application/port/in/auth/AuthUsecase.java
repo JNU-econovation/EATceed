79af9d92577d0d7052992ec5920fc637e8928afa
Jin Hyuk <ekrrdj07@gmail.com>
2024-10-08 01:56:20 +0900

    feat/#507: Service에 HttpServletRequest가 유출 되지 않도록 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/application/port/in/auth/AuthUsecase.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/application/port/in/auth/AuthUsecase.java
@@ -1,11 +1,17 @@
 package com.gaebaljip.exceed.application.port.in.auth;
 
+import com.gaebaljip.exceed.common.dto.HttpRequestDTO;
+import com.gaebaljip.exceed.common.dto.ReissueTokenDTO;
 import org.springframework.stereotype.Component;
 
 import com.gaebaljip.exceed.adapter.in.auth.request.LoginRequest;
 import com.gaebaljip.exceed.common.dto.LoginResponseDTO;
 
+import javax.servlet.http.HttpServletRequest;
+
 @Component
 public interface AuthUsecase {
     LoginResponseDTO execute(LoginRequest request);
+    ReissueTokenDTO reIssueToken(String accessToken, String refreshToken, HttpRequestDTO requestDTO);
+
 }
--- BE/exceed/src/main/java/com/gaebaljip/exceed/common/security/domain/JwtManager.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/security/domain/JwtManager.java
@@ -1,25 +1,25 @@
 package com.gaebaljip.exceed.common.security.domain;
 
-import java.security.Key;
-import java.time.LocalDateTime;
-import java.util.Date;
-
-import javax.servlet.http.HttpServletRequest;
-
 import com.gaebaljip.exceed.adapter.out.redis.RedisAdapter;
-import lombok.RequiredArgsConstructor;
+import com.gaebaljip.exceed.common.dto.HttpRequestDTO;
+import com.gaebaljip.exceed.common.security.exception.ExpiredJwtException;
+import com.gaebaljip.exceed.common.security.exception.InvalidJwtException;
+import com.gaebaljip.exceed.common.security.exception.UnSupportedJwtException;
+import io.jsonwebtoken.Header;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.MalformedJwtException;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
 
-import com.gaebaljip.exceed.common.security.exception.ExpiredJwtException;
-import com.gaebaljip.exceed.common.security.exception.InvalidJwtException;
-import com.gaebaljip.exceed.common.security.exception.UnSupportedJwtException;
-
-import io.jsonwebtoken.*;
-import io.jsonwebtoken.io.Decoders;
-import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
+import javax.servlet.http.HttpServletRequest;
+import java.security.Key;
+import java.time.LocalDateTime;
+import java.util.Date;
 
 @Component
 @Slf4j
@@ -111,46 +111,46 @@ public class JwtManager {
                 .compact();
     }
 
-    public boolean validateRefreshToken(String refreshToken, HttpServletRequest request)
+    public boolean validateRefreshToken(String refreshToken, HttpRequestDTO requestDTO)
             throws AuthenticationException {
         try {
             Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(refreshToken);
             log.info(
                     "method ={}, URL = {}, time={}, message={}",
-                    request.getMethod(),
-                    request.getRequestURL(),
+                    requestDTO.method(),
+                    requestDTO.url(),
                     LocalDateTime.now(),
                     "리프레시 토큰 검증 성공");
             return true;
         } catch (SecurityException | MalformedJwtException e) {
             log.error(
                     "method ={}, URL = {}, time={}, errorMessage={}",
-                    request.getMethod(),
-                    request.getRequestURL(),
+                    requestDTO.method(),
+                    requestDTO.url(),
                     LocalDateTime.now(),
                     e.getMessage());
             throw InvalidJwtException.EXECPTION; // 토큰의 서명이 유효하지 않은 경우
         } catch (io.jsonwebtoken.ExpiredJwtException e) {
             log.error(
                     "method ={}, URL = {}, time={}, errorMessage={}",
-                    request.getMethod(),
-                    request.getRequestURL(),
+                    requestDTO.method(),
+                    requestDTO.url(),
                     LocalDateTime.now(),
                     e.getMessage());
             throw ExpiredJwtException.EXECPTION; // 토큰이 만료된 경우
         } catch (io.jsonwebtoken.UnsupportedJwtException e) {
             log.error(
                     "method ={}, URL = {}, time={}, errorMessage={}",
-                    request.getMethod(),
-                    request.getRequestURL(),
+                    requestDTO.method(),
+                    requestDTO.url(),
                     LocalDateTime.now(),
                     e.getMessage());
             throw UnSupportedJwtException.EXECPTION; // 지원되지 않는 토큰
         } catch (IllegalArgumentException e) {
             log.error(
                     "method ={}, URL = {}, time={}, errorMessage={}",
-                    request.getMethod(),
-                    request.getRequestURL(),
+                    requestDTO.method(),
+                    requestDTO.url(),
                     LocalDateTime.now(),
                     e.getMessage());
         }
