1d134d3486711253dbfcf5f04bc76ee6b239c5e3
hwangdaesun <hds650588@gmail.com>
2024-10-08 00:33:49 +0900

    refactor/#522 : 클래스명 update -> find로 수정

--- BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/FindPasswordController.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/adapter/in/member/FindPasswordController.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import com.gaebaljip.exceed.adapter.in.member.request.SendEmailRequest;
-import com.gaebaljip.exceed.adapter.in.member.request.UpdatePasswordRequest;
+import com.gaebaljip.exceed.adapter.in.member.request.FindPasswordRequest;
 import com.gaebaljip.exceed.application.port.in.member.CheckCodeUsecase;
 import com.gaebaljip.exceed.application.port.in.member.GetCodeUsecase;
 import com.gaebaljip.exceed.application.port.in.member.PasswordValidationUsecase;
@@ -16,8 +16,8 @@ import com.gaebaljip.exceed.application.port.in.member.UpdatePasswordUsecase;
 import com.gaebaljip.exceed.common.ApiResponse;
 import com.gaebaljip.exceed.common.ApiResponse.CustomBody;
 import com.gaebaljip.exceed.common.ApiResponseGenerator;
-import com.gaebaljip.exceed.common.docs.member.UpdatePassword_updatePasswordExceptionDocs;
-import com.gaebaljip.exceed.common.docs.member.UpdatePassword_validateEmailExceptionDocs;
+import com.gaebaljip.exceed.common.docs.member.FindPassword_updatePasswordExceptionDocs;
+import com.gaebaljip.exceed.common.docs.member.FindPassword_validateEmailExceptionDocs;
 import com.gaebaljip.exceed.common.swagger.ApiErrorExceptionsExample;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -28,7 +28,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @RequestMapping("/v1")
 @Tag(name = "[비밀번호 찾기]")
-public class UpdatePasswordController {
+public class FindPasswordController {
 
     private final PasswordValidationUsecase passwordValidationUsecase;
     private final GetCodeUsecase getCodeUsecase;
@@ -43,7 +43,7 @@ public class UpdatePasswordController {
             summary = "비밀번호 찾기 전 이메일 검증 및 메일 전송",
             description = "비밀번호 찾기 전, 이메일 검증 및 이메일을 재전송한다.")
     @PostMapping("/email")
-    @ApiErrorExceptionsExample(UpdatePassword_validateEmailExceptionDocs.class)
+    @ApiErrorExceptionsExample(FindPassword_validateEmailExceptionDocs.class)
     public ApiResponse<CustomBody<Void>> validateEmail(
             @RequestBody @Valid SendEmailRequest request) {
         passwordValidationUsecase.execute(request.email());
@@ -64,9 +64,9 @@ public class UpdatePasswordController {
             summary = "비밀번호 찾기",
             description = "비밀번호를 찾을 때 사용한다. 단, 비밀번호 찾기 버튼을 누르는 것이지 실제로는 새로운 비밀번호로 수정한다.")
     @PatchMapping("/members/password")
-    @ApiErrorExceptionsExample(UpdatePassword_updatePasswordExceptionDocs.class)
-    public ApiResponse<CustomBody<Void>> updatePassword(
-            @RequestBody @Valid UpdatePasswordRequest request) {
+    @ApiErrorExceptionsExample(FindPassword_updatePasswordExceptionDocs.class)
+    public ApiResponse<CustomBody<Void>> findPassword(
+            @RequestBody @Valid FindPasswordRequest request) {
         passwordValidationUsecase.execute(request.email());
         checkCodeUsecase.execute(request.email(), request.code());
         updatePasswordUsecase.execute(request.email(), request.newPassword());
--- BE/exceed/src/main/java/com/gaebaljip/exceed/common/docs/member/FindPassword_validateEmailExceptionDocs.java
+++ BE/exceed/src/main/java/com/gaebaljip/exceed/common/docs/member/FindPassword_validateEmailExceptionDocs.java
@@ -2,21 +2,17 @@ package com.gaebaljip.exceed.common.docs.member;
 
 import com.gaebaljip.exceed.common.exception.EatCeedException;
 import com.gaebaljip.exceed.common.exception.member.EmailNotVerifiedException;
-import com.gaebaljip.exceed.common.exception.member.ExpiredCodeException;
 import com.gaebaljip.exceed.common.exception.member.MemberNotFoundException;
 import com.gaebaljip.exceed.common.swagger.ExceptionDoc;
 import com.gaebaljip.exceed.common.swagger.ExplainError;
 import com.gaebaljip.exceed.common.swagger.SwaggerExampleExceptions;
 
 @ExceptionDoc
-public class UpdatePassword_updatePasswordExceptionDocs implements SwaggerExampleExceptions {
+public class FindPassword_validateEmailExceptionDocs implements SwaggerExampleExceptions {
 
     @ExplainError("회원이 존재하지 않을 때")
     public EatCeedException 회원이_존재하지_않을_때 = MemberNotFoundException.EXECPTION;
 
     @ExplainError("이메일 인증을 수행하지 않은 회원일 때")
     public EatCeedException 이메일인증을_수행하지_않고_비밀번호_변경을_시도할때 = EmailNotVerifiedException.EXECPTION;
-
-    @ExplainError("잘못된(혹은 만료된) 인증 코들일 경우")
-    public EatCeedException 잘못된_인증코드일_경우 = ExpiredCodeException.EXECPTION;
 }
